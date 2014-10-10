e2e52cfa862ea4a8b0a13abf3ebd16a812145143
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2014-10-10 14:34:11 +0000

    ILL: Initial Angular Work

---
+++ Koha/ILL.pm
@@ -0,0 +1,93 @@
+package Koha::ILL;
+
+# Copyright PTFS Europe 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use Carp;
+
+use BLDSS;
+use XML::LibXML;
+use Data::Dumper;
+
+=head1 NAME
+
+Koha::ILL - Koha ILL Object Class
+
+=head1 SYNOPSIS
+
+use Koha::ILL;
+
+my $bldss = Koha::ILL->new($service);
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+# New, Initialises object (with config if given)
+=head3 new
+
+=cut
+
+sub new {
+    my ( $class, $service ) = @_;
+    my $self = {};
+
+    bless( $self, $class );
+    return $self;
+}
+#
+# Config, returns config hashref (and allows manipulation of config if passed)
+
+# Search, Searches using API
+=head3 search
+
+my $results = $bldss->search($query);
+
+=cut
+
+sub search {
+    my ( $self, $query ) = @_;
+
+    my $api = BLDSS->new();
+    my $reply = $api->search($query);
+
+    my $parser = XML::LibXML->new();
+    my $doc = $parser->load_xml( { string => $reply } );
+
+    my $response = {};
+    foreach my $record ( $doc->findnodes('/apiResponse/result/records/record') ) {
+        for my $property ( $record->findnodes('./*') ) {
+            if ( $property->findnodes('*')->size < 1 ) {
+                $response->{"$property->nodeName()"} = $property->textContent();
+            } else {
+                $response->{"metadata"}->{"$property->nodeName()"} = $property->textContent();
+            }
+        }
+    }
+
+    return $response;
+}
+
+#
+# Request, Submits request using API
+#
+# Status, Get status update using API
+
+1;
---
+++ koha-tmpl/intranet-tmpl/prog/en/js/pages/ill.js
@@ -0,0 +1,14 @@
+(function() {
+    var app = angular.module('ill', [ ]);
+
+    app.controller( 'searchController', [ '$scope', '$http', function($scope, $http){
+        $scope.submit = function() {
+            console.log($scope.query);
+            $http.get('/cgi-bin/koha/svc/ill?query='+$scope.query).success( function(reply) {
+                $scope.results = reply;
+                console.log(reply);
+            });
+        };
+    } ]);
+
+})();
