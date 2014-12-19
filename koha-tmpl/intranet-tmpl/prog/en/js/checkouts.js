b5ca2186a1eb40a24ecff9a22e269351db8fbb69
David Roberts <david.roberts@ptfs-europe.com>
2014-12-19 13:12:23 +0000

    CHES: Correct broken display of relatives checkouts

--- koha-tmpl/intranet-tmpl/prog/en/js/checkouts.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/checkouts.js
@@ -465,7 +465,8 @@ $(document).ready(function() {
                             title += "</a></span>";
 
                             if ( oObj.author ) {
-                                title += " " + BY + " " + oObj.author;
+                              //  title += " " + BY + " " + oObj.author;
+                                 title += " " + BY.replace( "_AUTHOR_",  " " + oObj.author );
                             }
 
                             if ( oObj.itemnotes ) {
