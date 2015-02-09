0acfceebd3e5b942e019c1176e4b1d460b839d1f
Jonathan Field <jonathan.field@ptfs-europe.com>
2015-02-09 17:11:01 +0000

    SIP Extension for reporting child user parental permissions... Icam

--- C4/SIP/Sip/MsgType.pm
+++ C4/SIP/Sip/MsgType.pm
@@ -996,6 +996,7 @@ sub handle_patron_info {
 
         # Custom protocol extension to report patron internet privileges
         $resp .= maybe_add(FID_INET_PROFILE,     $patron->inet_privileges);
+        $resp .= maybe_add(FID_PARENTAL_PERMISSION,     $patron->parental_permission);
 
         $resp .= maybe_add( FID_SCREEN_MSG, $patron->screen_msg, $server );
         $resp .= maybe_add( FID_SCREEN_MSG, $patron->{branchcode}, $server )
