3aa70bd65c7820922c55d8a5bbb7d282bf42f5d1
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2015-02-12 19:28:22 +0100

    Create new advanced search page.
    * ill/ill-new.pl: new file.
    * koha-tmpl/intranet-tmpl/prog/en/modules/ill/ill-new.tt: new file.

---
+++ ill/ill-new.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+
+# Copyright 2013 PTFS-Europe Ltd and Mark Gavillet
+# Copyright 2014 PTFS-Europe Ltd
+#
+# This file is part of Koha.
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use CGI;
+use C4::Auth;
+use C4::Output;
+use C4::Search qw(GetDistinctValues);
+use C4::Context;
+use Koha::ILLRequests;
+use URI::Escape;
+
+my $input = CGI->new;
+my $reply = [];
+my $type = $input->param('query_type') || 'new';
+my $query = $input->param('query_value') || '';
+
+my ( $template, $borrowernumber, $cookie )
+  = get_template_and_user(
+                          {
+                           template_name => 'ill/ill-new.tt',
+                           query         => $input,
+                           type          => 'intranet',
+                           flagsrequired => { ill => 1 },
+                          }
+                         );
+
+$template->param( query_value => $query );
+$template->param( query_type => $type );
+
+if ( $type eq 'search' and $query ) {
+    my $opts = {};
+    my $nav_qry = "?query_type=search&query_value=" . uri_escape($query);
+    for my $opt qw( isbn issn title author start_rec max_results ) {
+        my $val = $input->param($opt);
+        if ( $val ne '' ) {
+            $opts->{$opt} = $val;
+            $nav_qry .= "&${opt}=" . uri_escape($val);
+        }
+    }
+    $reply = Koha::ILLRequests->new()->search_api($query, $opts);
+    my $max_results = $opts->{max_results} || 10;
+    my $results = @{$reply};
+    my $bcounter = $input->param('start_rec') || 1;
+    my $ncounter = $bcounter + $results;
+    my $pcounter = $bcounter - $results;
+    my $next = 0;
+    $next = $nav_qry . "&start_rec=" . $ncounter
+      if ( $results == $max_results );
+    my $prev = 0;
+    $prev = $nav_qry . "&start_rec=" . $pcounter
+      if ( $pcounter > 1 ) ;
+    $template->param( next => $next );
+    $template->param( prev => $prev );
+
+} elsif ( $type eq 'new' ) {
+    # No special action.
+} else {
+    # No special action.
+}
+
+$template->param( recv => $input );
+$template->param( reply => $reply );
+$template->param( debug => 1 ); # if ( $input->param('debug') );
+
+output_html_with_http_headers( $input, $cookie, $template->output );
