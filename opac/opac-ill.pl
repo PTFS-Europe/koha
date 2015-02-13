bddb79a3acec2de64b00a82352d598489a2ff02a
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2015-02-13 21:45:19 +0100

    Add OPAC ILL interface.
    * koha-tmpl/opac-tmpl/bootstrap/en/includes/usermenu.inc: add ILL menue
      if applicable.
    * koha-tmpl/opac-tmpl/bootstrap/en/modules/opac-ill.tt: new file.
    * opac/opac-ill.pl: rewrite for new ILL.

--- opac/opac-ill.pl
+++ opac/opac-ill.pl
@@ -18,8 +18,7 @@
 # with Koha; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-use strict;
-use warnings;
+use Modern::Perl;
 
 use CGI;
 
@@ -27,14 +26,17 @@ use C4::Auth;
 use C4::Context;
 use C4::Koha;
 use C4::Output;
+use C4::Branch;
 use Koha::Borrowers;
+use Koha::ILLRequests;
+use URI::Escape;
 
-my $query = CGI->new();
+my $cgi = CGI->new();
 
 my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
     {
         template_name   => 'opac-ill.tt',
-        query           => $query,
+        query           => $cgi,
         type            => 'opac',
         authnotrequired => 0,
         flagsrequired   => { borrow => 1 },
@@ -42,8 +44,94 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
     }
 );
 
-my $borrower = Koha::Borrower->new()->Find( { borrowernumber => $borrowernumber } );
-my @requests = $borrower->ILLRequests();
-$template->param( ILLRequests => \@requests );
+my $reply;
+my $query    = $cgi->param('query_value');
+my $here     = "/cgi-bin/koha/opac-ill.pl";
+my $op       = $cgi->param('op');
+my $error    = 0;
+my $borrower = Koha::Borrowers->new->find($borrowernumber);
 
-output_html_with_http_headers( $query, $cookie, $template->output );
+if ( fail(1) ) {
+    $error = {
+        error => "missing_fields",
+        op    => $op,
+    };
+
+} elsif ( $op eq 'new' ) {
+    $template->param(
+        branches => GetBranchesLoop($borrower->branchcode),
+        type     => [ "Book", "Article", "Journal" ],
+        back     => $here,
+        forward  => $here . "?op=search",
+    );
+
+} elsif ( $op eq 'search' ) {
+    my $opts = {};
+    my $nav_qry = "?op=search&query_value=" . uri_escape($query);
+    for my $opt qw( isbn issn title author type start_rec max_results ) {
+        my $val = $cgi->param($opt);
+        if ( !fail($val)) {
+            $opts->{$opt} = $val;
+            $nav_qry .= "&${opt}=" . uri_escape($val);
+        }
+    }
+    $reply = Koha::ILLRequests->new()->search_api($query, $opts);
+    my $max_results = $opts->{max_results} || 10;
+    my $results  = @{$reply};
+    my $bcounter = $cgi->param('start_rec') || 1;
+    my $ncounter = $bcounter + $results;
+    my $pcounter = $bcounter - $results;
+    my $next = 0;
+    $next = $nav_qry . "&start_rec=" . $ncounter
+      if ( $results == $max_results );
+    my $prev = 0;
+    $prev = $nav_qry . "&start_rec=" . $pcounter
+      if ( $pcounter > 1 ) ;
+    my $rq_qry = "?op=request";
+    $rq_qry .= "&query_value=";
+    $template->param(
+        back        => $here . "?op=search",
+        forward     => $here . "?op=request",
+        next        => $next,
+        prev        => $prev,
+        rqp         => $rq_qry,
+        query_value => $query,
+    );
+} elsif ( $op eq 'request' ) {
+    my $request = Koha::ILLRequests->new->request( {
+        uin      => $query,
+        branch   => $borrower->branchcode,
+        borrower => $borrower->borrowernumber,
+    } );
+    if ($request) {
+        my $requests = Koha::ILLRequests->new->retrieve_ill_requests($borrowernumber);
+        foreach my $rq ( @{$requests} ) {
+            push @{$reply}, $rq->getSummary;
+        }
+    }
+
+} else {
+    my $requests = Koha::ILLRequests->new->retrieve_ill_requests($borrowernumber);
+    if ($requests) {
+        foreach my $rq ( @{$requests} ) {
+            push @{$reply}, $rq->getSummary;
+        }
+    }
+}
+
+$template->param(
+    illview  => 1,
+    reply    => $reply,
+    error    => $error,
+    op       => $op,
+);
+
+output_html_with_http_headers( $cgi, $cookie, $template->output );
+
+sub fail {
+    my @values = @_;
+    foreach my $val ( @values ) {
+        return 1 if (!$val or $val eq '');
+    }
+    return 0;
+}
