06d4a2dd6676bcdc5d946b9a01c35c0f4f40116d
Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
2015-02-13 19:11:01 +0100

    Preliminary ILL systempreferences.
    * installer/data/mysql/atomicupdate/ill_schema.sql: update schema.
    * koha-tmpl/intranet-tmpl/prog/en/modules/admin/preferences/admin.pref
      (Administration): add Interlibrary Loans options.

--- installer/data/mysql/atomicupdate/ill_schema.sql
+++ installer/data/mysql/atomicupdate/ill_schema.sql
@@ -2,28 +2,10 @@
 INSERT INTO systempreferences (variable,value,explanation,options,type)
 VALUES ('ILLModule','0','If ON, enables the Inter-Library Loan module','','YesNo');
 INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('BookILLRequest', '0', 'If ON, enables the requesting of books in the Inter-Library Loan module', '', 'YesNo');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('JournalILLRequest', '0', 'If ON, enables the requesting of journals in the Inter-Library Loan module', '', 'YesNo');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('ThesisILLRequest','0','If ON, enables the requesting of theses in the Inter-Library Loan module','','YesNo');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('OtherILLRequest','0','If ON, enables the requesting of generic items in the Inter-Library Loan module','','YesNo');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('ILLRequestPrefix','ILLREQ-','Prefix for the ILL request ID','','free');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
 VALUES ('OpacILLRequests','0','If ON, allows patrons to view their ILL requests in the OPAC','','YesNo');
 INSERT INTO systempreferences (variable,value,explanation,options,type)
 VALUES ('ILLEmailNotify','0','If ON, patrons receive an email notification of a placed request','','YesNo');
-INSERT INTO systempreferences (variable,value,explanation,options,type)
-VALUES ('ILLNewRequestStatus','NEW',' Default status for new ILL requests','NEW','Choice');
 
-INSERT INTO systempreferences (variable, explanation, type)
-VALUES ('ILLLocalField1', 'Custom field for ILL submission forms', 'free');
-INSERT INTO systempreferences (variable, explanation, type)
-VALUES ('ILLLocalField2', 'Custom field for ILL submission forms', 'free');
-INSERT INTO systempreferences (variable, explanation, type)
-VALUES ('ILLLocalField3', 'Custom field for ILL submission forms', 'free');
 -- Permissions
 
 -- Userflags
