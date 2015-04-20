0dc8054c0b7028ff5da30916c164e20f238e2fa4
Colin Campbell <colin.campbell@ptfs-europe.com>
2015-04-20 13:57:23 +0100

    Be consistent on checking userenv in Accounts

--- C4/Accounts.pm
+++ C4/Accounts.pm
@@ -94,7 +94,8 @@ sub recordpayment {
     my $dbh        = C4::Context->dbh;
     my $newamtos   = 0;
     my $accdata    = "";
-    my $branch     = C4::Context->userenv->{'branch'};
+    my $branch;
+    $branch     = C4::Context->userenv->{'branch'} if C4::Context->userenv;
     my $amountleft = $data;
     my $manager_id = 0;
     $manager_id = C4::Context->userenv->{'number'} if C4::Context->userenv;
