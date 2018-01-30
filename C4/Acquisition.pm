8cd0ee236ca44f334182dcd70dc7f1143f830a54
Nick Clemens <nick@bywatersolutions.com>
2018-01-30 15:06:31 +0000

    Bug 18736: (follow-up) fix wrong call to _get_rounding_sql

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -2183,11 +2183,10 @@ sub GetLateOrders {
         AND aqbasket.closedate IS NOT NULL
         AND (aqorders.datecancellationprinted IS NULL OR aqorders.datecancellationprinted='0000-00-00')
     ";
-    my ($round_sql_start,$round_sql_end) = _get_rounding_sql();
     if ($dbdriver eq "mysql") {
         $select .= "
         aqorders.quantity - COALESCE(aqorders.quantityreceived,0)                 AS quantity,
-        (aqorders.quantity - COALESCE(aqorders.quantityreceived,0)) * $round_sql_start aqorders.rrp $round_sql_end AS subtotal,
+        (aqorders.quantity - COALESCE(aqorders.quantityreceived,0)) * " . _get_rounding_sql("aqorders.rrp") . " AS subtotal,
         DATEDIFF(CAST(now() AS date),closedate) AS latesince
         ";
         if ( defined $delay ) {
@@ -2199,7 +2198,7 @@ sub GetLateOrders {
         # FIXME: account for IFNULL as above
         $select .= "
                 aqorders.quantity                AS quantity,
-                aqorders.quantity * $round_sql_start aqorders.rrp $round_sql_end AS subtotal,
+                aqorders.quantity * "._get_rounding_sql("aqorders.rrp")." AS subtotal,
                 (CAST(now() AS date) - closedate)            AS latesince
         ";
         if ( defined $delay ) {
