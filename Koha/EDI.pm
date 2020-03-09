9d6ffa82a2826e8eec04702e0f91df202b5b300b
Colin Campbell <colin.campbell@ptfs-europe.com>
2020-03-09 14:08:25 +0000

    (chester) Map GIR LSQ To ccode
    map LSQ to ccode rather than default location

--- Koha/EDI.pm
+++ Koha/EDI.pm
@@ -822,7 +822,7 @@ sub quote_item {
                     my $new_item = {
                         itype =>
                           $item->girfield( 'stock_category', $occurrence ),
-                        location =>
+                        ccode =>
                           $item->girfield( 'collection_code', $occurrence ),
                         itemcallnumber =>
                           $item->girfield( 'shelfmark', $occurrence )
@@ -909,7 +909,7 @@ sub quote_item {
                         replacementprice => $price,
                         itype =>
                           $item->girfield( 'stock_category', $occurrence ),
-                        location =>
+                        ccode =>
                           $item->girfield( 'collection_code', $occurrence ),
                         itemcallnumber =>
                           $item->girfield( 'shelfmark', $occurrence )
@@ -1124,7 +1124,7 @@ sub _create_item_from_quote {
     $item_hash->{booksellerid} = $quote->vendor_id;
     $item_hash->{price}        = $item_hash->{replacementprice} = $item->price;
     $item_hash->{itype}        = $item->girfield('stock_category');
-    $item_hash->{location}     = $item->girfield('collection_code');
+    $item_hash->{ccode}     = $item->girfield('collection_code');
 
     my $note = {};
 
