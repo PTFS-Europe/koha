14e05ea71207a63a39aec5263077b7d7091e365f
PTFS Admin <hosting@ptfs-europe.com>
2020-07-10 13:14:46 +0100

    HERTS customisations to be added after every upgrade(JT)

---
+++ opac/UH/showLoansADFS.pl
@@ -0,0 +1,146 @@
+#!/usr/bin/perl -w
+
+use CGI qw/:standard -debug/;
+use LWP::UserAgent;
+use HTTP::Request;
+use XML::LibXML;
+use Data::Dumper;
+use JSON;
+
+use C4::Auth;
+use C4::Auth_with_shibboleth;
+
+my $DEBUG = 0;
+
+my $q = new CGI;
+#my $userBarcode = $q->param('ubarcode');
+#chomp($userBarcode);
+
+my ( $user, $cookie, $sessionID, $flags ) = checkauth( $q, 0, {}, 'opac' );
+my $shib_login = get_login_shib();
+my ( $retval, $userBarcode, $userID ) = checkpw_shib( $shib_login );
+
+my $serviceBaseURL = 'https://library.herts.ac.uk/cgi-bin/koha/ilsdi.pl';
+
+my $apiURL = &getLookupPatronURL($userBarcode,$serviceBaseURL);
+my $response = &webCall($apiURL);
+
+my $json;
+
+if ($response !~ m/^ERROR/) {
+	my $kohaID = &parseLookupResponse($response);
+	print "The Koha ID is $kohaID\n" if $DEBUG;
+	my $userInfoURL = &getPatronInformationURL($kohaID,$serviceBaseURL);
+	my $res = &webCall($userInfoURL);
+	$json = &parseXML($res);
+ }
+ else {
+	# create JSON with error in
+}
+
+print $q->header(
+	-type => 'application/json'
+);
+#print "Content-type: application/json\n\n";
+print $json;
+
+exit;
+
+#################################
+
+sub getLookupPatronURL($$) {
+
+	my $barcode = $_[0];
+	my $baseURL = $_[1];
+	my $returnURL = $baseURL . "?service=LookupPatron&id=" . $barcode . "&id_type=cardnumber";
+	print "The LookupPatron url is $returnURL\n" if $DEBUG;
+	return $returnURL;
+}
+
+#################################
+
+sub parseLookupResponse($) {
+
+	my $response = $_[0];
+	$response =~ m/<id>(.*)<\/id>/;
+	my $kohaUserID = $1; # need to check for error here
+	return $kohaUserID;
+}
+
+##################################
+
+sub getPatronInformationURL($$) {
+
+	my $kuid = $_[0];
+	my $baseURL = $_[1];
+	my $returnURL = $baseURL . "?service=GetPatronInfo&patron_id=" . $kuid . "&show_contact=0&show_loans=1";
+	print "The GetPatronInfo url is $returnURL\n" if $DEBUG;
+	return $returnURL;
+}
+
+##################################
+
+sub webCall($) {
+
+	my $apiURL = $_[0];
+	my $ua = LWP::UserAgent->new;
+	my $request = HTTP::Request->new("GET",$apiURL);
+	print $request->as_string if $DEBUG;
+	my $response = $ua->request($request);
+
+	my $returnVal;
+
+	if ($response->is_success) {
+		print $response->decoded_content if $DEBUG; # or whatever
+		$returnVal = $response->decoded_content;
+	}	else {
+		print $response->decoded_content if $DEBUG;
+		# die $response->status_line;
+		$returnVal = "ERROR: " . $response->status_line;
+	}
+
+	return $returnVal;;
+}
+
+###################################
+
+sub parseXML($) {
+
+	my @loans;
+	$xmlData = $_[0];
+	my $parser = XML::LibXML->new();
+	my $doc = $parser->parse_string($xmlData);
+	foreach my $loan ($doc->findnodes('/GetPatronInfo/loans/loan')) {
+
+		$hashRef{"title"} = $loan->findvalue('./title');
+		$hashRef{"datedue"} = $loan->findvalue('./date_due');
+		if ($loan->findvalue('./overdue')) {
+			$hashRef{"overdue"} = $loan->findvalue('./overdue');
+		} else {
+			$hashRef{"overdue"} = "0";
+		}
+		$hashRef{"totalrenewals"} = $loan->findvalue('./totalrenewals');
+
+		push @loans, { %hashRef };
+		%hashRef = ();
+	}
+
+	if ($DEBUG) {
+		my $count = 1;
+		foreach my $loan (@loans) {
+			print Dumper($loan);
+			print "Loan $count : ";
+			foreach my $key (keys %$loan) {
+				print $key . " : " . ${$loan}{$key} . "\n";
+			}
+			$count++;
+		}
+	}
+
+	my $json_text = JSON->new->utf8->encode(\@loans);
+	#my $JSON = JSON->new->utf8;
+	#$JSON->allow_blessed(1);
+	#my $json_text = $JSON->encode(\@loans);
+	#print $json_text if $DEBUG;
+	return $json_text;
+}
---
+++ opac/UH/showLoansTest.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl -w
+
+use CGI qw/:standard -debug/;
+use Data::Dumper;
+use JSON;
+use DBI;           # Connect to DB
+use DBD::mysql;    # Manipulate MySQL
+
+use C4::Context;    # Koha Database Access
+
+my $DEBUG = 0;
+
+my $q = new CGI;
+my $userBarcode = $q->param('barcode');
+chomp($userBarcode);
+
+my $data = &getData($userBarcode);
+# print Dumper($data);
+my $json = &parseData($data);
+
+print $q->header(
+	-type => 'application/json'
+);
+print $json;
+
+exit;
+
+###################################
+
+sub parseData($) {
+
+	my $data = $_[0];
+	my @loans;
+
+	foreach my $rec (@$data) {
+		my ($bibID,$onHold,$numberOfRenewals,$title,$overdue,$returnDate,$maxRenewals) = @$rec;
+
+		if ($onHold > 0) {
+			$onHold = 1;
+		}
+
+		$hashRef{"title"} = $title;
+		$hashRef{"datedue"} = $returnDate;
+		$hashRef{"biblionumber"} = $bibID;
+		$hashRef{"overdue"} = $overdue;
+		$hashRef{"totalrenewals"} = $numberOfRenewals;
+		$hashRef{"hold"} = $onHold;
+		$hashRef{"maxRenewals"} = $maxRenewals;
+		push @loans, { %hashRef };
+		%hashRef = ();
+	}
+
+	my $json_text = JSON->new->utf8->encode(\@loans);
+
+	#my $JSON = JSON->new->utf8;
+	#$JSON->allow_blessed(1);
+	#my $json_text = $JSON->encode(\@loans);
+	#print $json_text if $DEBUG;
+	return $json_text;
+}
+
+##########################################
+
+sub getData($) {
+
+	my $barcode = $_[0];
+
+	my $dbh = C4::Context->dbh;
+    my $query;
+    my $sth;
+
+	#my $SQL = "select bi.biblionumber,
+	#			(select count(*) from reserves r where r.biblionumber = bi.biblionumber),
+	#			i.renewals,
+	#			SUBSTRING_INDEX(ExtractValue(bm.metadata,'//datafield[\@tag=\"245\"]/subfield[\@code>=\"a\"]'),'/','1'),
+	#			if (curdate() > i.date_due,1,0),
+	#			date(i.date_due)
+	#			from issues i, biblioitems bi, items it, biblio_metadata bm
+	#			where i.borrowernumber = (select borrowernumber from borrowers where (cardnumber = '". $barcode . "' or userid = '". $barcode . "')) 
+	#			and i.itemnumber = it.itemnumber
+	#			and bm.biblionumber = bi.biblionumber
+	#			and it.biblioitemnumber = bi.biblioitemnumber
+	#";
+
+	my $SQL = "select bi.biblionumber,
+       			(select count(*) from reserves r where r.biblionumber = bi.biblionumber),
+       			i.renewals,
+			SUBSTRING_INDEX(ExtractValue(bm.metadata,'//datafield[\@tag=\"245\"]/subfield[\@code>=\"a\"]'),'/','1'),
+       			if (curdate() > i.date_due,1,0),
+       			date(i.date_due),
+       			if(b.categorycode = 'UHSTU','20','60') maxRenewals
+			from issues i, biblioitems bi, items it, biblio_metadata bm, borrowers b
+			where i.borrowernumber = b.borrowernumber
+			and (cardnumber = '". $barcode . "' or userid = '". $barcode . "') 
+			and i.itemnumber = it.itemnumber
+			and bm.biblionumber = bi.biblionumber
+			and it.biblioitemnumber = bi.biblioitemnumber
+	";
+
+	# print "checkHolds sub: The SQL is $SQL";
+
+	$sth = $dbh->prepare($SQL)
+		or warn "Can't prepare $query: $dbh->errstr\n";
+	$sth->execute()
+          or warn "Can't execute the query: $sth->errstr\n";
+
+	my $data = $sth->fetchall_arrayref();
+
+	return $data;
+}
