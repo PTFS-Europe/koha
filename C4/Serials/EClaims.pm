d72f0104c0c41b2be8b1d7c5c28f40872c533b50
Colin Campbell <colin.campbell@ptfs-europe.com>
2020-12-18 14:01:59 +0000

    Add C4::Serials::EClaims

---
+++ C4/Serials/EClaims.pm
@@ -0,0 +1,30 @@
+package C4::Serials::EClaims;
+use strict;
+use warnings;
+
+sub format_claim {
+    my $message = q{};
+    # UNB
+    # UNH
+    # BGM
+    # DTM
+    # NAD+BY
+    # NAD+SU
+    # DOC
+    # LIN
+    # PIA
+    # IMD (050/109/080)
+    # STS
+    # RFF+LI
+    # RFF+SNA
+    # RFF+SNL
+    # RFF+ACT
+    # DTM
+    # QTY
+    # UNT
+    # UNZ
+    return $message;
+}
+
+1;
+
