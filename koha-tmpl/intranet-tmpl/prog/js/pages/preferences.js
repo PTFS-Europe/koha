797d6f8775e90f7dfc04caf1bd55004dcf67aaba
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-01-11 14:50:17 +0000

    Bug 27378: Add new 'modaljs' syspref type
    This commit adds a new 'modaljs' syspref type. This is similar to the existing
    'modalselect' syspref type but instead of using a file to create a list of
    selectable options which are displayed in the modal, it assumes that anything
    can happen within the modal driven by JS, and whatever happens is going to
    return a string, that will be saved as the syspref value. In our case, the
    string will be all the details of the “chunks” concatenated into a string
    
    Signed-ff-by: Barry Cannon <bc@interleaf.ie>

--- koha-tmpl/intranet-tmpl/prog/js/pages/preferences.js
+++ koha-tmpl/intranet-tmpl/prog/js/pages/preferences.js
@@ -260,6 +260,10 @@ $( document ).ready( function () {
         $("#prefModal").modal("show");
     });
 
+    $(".modaljs").on("click", function () {
+        console.log('modaljs clicked');
+    });
+
     $("#saveModalPrefs").on("click", function(){
         var formfieldid = $("#" + $(this).data("target") );
         var prefs = [];
