4e987244b05ff1aa894ea87b5bdfd1ae115a469e
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-01-25 11:18:29 +0000

    Bug 27542: Allow cancellation of partner ILL
    This commit enables the display of the "Revert request" button when a
    request's status is GENREQ / "Requested from partners"
    
    The relevant unit test has also been updated

--- Koha/Illrequest.pm
+++ Koha/Illrequest.pm
@@ -436,11 +436,11 @@ sub _core_status_graph {
             name           => 'Requested from partners',
             ui_method_name => 'Place request with partners',
             method         => 'generic_confirm',
-            next_actions   => [ 'COMP' ],
+            next_actions   => [ 'COMP', 'REQREV' ],
             ui_method_icon => 'fa-send-o',
         },
         REQREV => {
-            prev_actions   => [ 'REQ' ],
+            prev_actions   => [ 'REQ', 'GENREQ' ],
             id             => 'REQREV',
             name           => 'Request reverted',
             ui_method_name => 'Revert Request',
