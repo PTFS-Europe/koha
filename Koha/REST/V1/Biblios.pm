7c3b39c8c21b15de2df5876a0b24cac79ce38c2f
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-09-29 15:17:38 +0100

    Bug 29002: Add ability to book material from the staff client
    This patch introduces a new modal to the biblio details page to allow
    booking of materials.
    
    Test plan
    1) Navigate to the details page of a biblio
    2) Note the new 'Place booking' button in the toolbar
    3) Click the new button and note the new modal dialogue
    4) Enter part of a patron name or cardnumber and then select from the
       presented results
    5) Optionally pick an item from the select list
    6) Select a start date and end date from the calender
    7) Submit
    8) Attempt to book the same item to another user, note that the dates
       previously selected are now greyed out.
    9) Experiment with different items and all items options to confirm the
       available slots in the datepicker update as expected.
    10) Sign off
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Janet McGowan <janet.mcgowan@ptfs-europe.com>
    Signed-off-by: Caroline Cyr La Rose <caroline.cyr-la-rose@inlibro.com>
    Signed-off-by: Laurence Rault <laurence.rault@biblibre.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit d6a8f65f147e17770e827473d898fde4906b3f07)

--- Koha/REST/V1/Biblios.pm
+++ Koha/REST/V1/Biblios.pm
@@ -254,7 +254,7 @@ Controller function that handles retrieving biblio's bookings
 sub get_bookings {
     my $c = shift->openapi->valid_input or return;
 
-    my $biblio = Koha::Biblios->find( { biblionumber => $c->validation->param('biblio_id') }, { prefetch => ['bookings'] } );
+    my $biblio = Koha::Biblios->find( { biblionumber => $c->param('biblio_id') }, { prefetch => ['bookings'] } );
 
     unless ( $biblio ) {
         return $c->render(
@@ -288,7 +288,8 @@ Controller function that handles retrieving biblio's items
 sub get_items {
     my $c = shift->openapi->valid_input or return;
 
-    my $biblio = Koha::Biblios->find( { biblionumber => $c->validation->param('biblio_id') }, { prefetch => ['items'] } );
+    my $biblio = Koha::Biblios->find( { biblionumber => $c->param('biblio_id') }, { prefetch => ['items'] } );
+    my $bookable_only = $c->param('bookable');
 
     unless ( $biblio ) {
         return $c->render(
@@ -302,6 +303,7 @@ sub get_items {
     return try {
 
         my $items_rs = $biblio->items;
+        $items_rs = $items_rs->filter_by_bookable if $bookable_only;
         my $items    = $c->objects->search( $items_rs );
         return $c->render(
             status  => 200,
