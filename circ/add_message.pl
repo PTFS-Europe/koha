f01f310faf1cdc86c8af6ee9db93d5ccd8fd0441
David Cook <dcook@prosentient.com.au>
2022-04-20 01:53:26 +0000

    Bug 22990: Follow-up to fix merge mistakes

--- circ/add_message.pl
+++ circ/add_message.pl
@@ -22,7 +22,7 @@ use Modern::Perl;
 use CGI qw ( -utf8 );
 
 use C4::Auth qw( get_template_and_user );
-use C4::Output;
+use C4::Output qw( output_and_exit_if_error );
 use Koha::Patron::Message;
 
 my $input = CGI->new;
