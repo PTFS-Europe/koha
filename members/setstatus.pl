4645c5311e7a615324ea717b67c524c50a4e21dc
David Cook <dcook@prosentient.com.au>
2022-04-20 01:53:26 +0000

    Bug 22990: Follow-up to fix merge mistakes
    (cherry picked from commit 927475387a83a7f52ce3315eacc7161c687dbabd)

--- members/setstatus.pl
+++ members/setstatus.pl
@@ -53,7 +53,15 @@ my $patron         = Koha::Patrons->find( $borrowernumber );
 if ( $logged_in_user->can_see_patron_infos($patron) ) {
     if ( $reregistration eq 'y' ) {
         # re-reregistration function to automatic calcul of date expiry
-        output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
+        #FIXME: Can't use output_and_exit_if_error here because there's no $template available
+        #output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
+        die "Wrong CSRF token" unless Koha::Token->new->check_csrf(
+            {
+            session_id => scalar $input->cookie('CGISESSID'),
+            token      => scalar $input->param('csrf_token'),
+            }
+        );
+
         $dateexpiry = $patron->renew_account;
     } else {
         my $sth = $dbh->prepare("UPDATE borrowers SET debarred = ?, debarredcomment = '' WHERE borrowernumber = ?");
