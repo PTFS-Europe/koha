8e836d6b0bed0512be2f8ba27e12ad4138b0ed57
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2022-06-21 15:45:55 +0100

    Bug 30719: Batch statuses
    This commits adds the following provision for batch statuses:
    
    - Database updates
    - Object definitions
    - CRUD REST API
    - Unit tests
    - UI adding support for batch statuses in batch UI
    - Admin UI for managing batch statuses

--- installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
+++ installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
@@ -7,14 +7,25 @@ return {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
-            CREATE TABLE `illbatches` (
+            CREATE TABLE IF NOT EXISTS `illbatches` (
                 `id` int(11) NOT NULL auto_increment, -- Batch ID
                 `name` varchar(100) NOT NULL,         -- Unique name of batch
                 `backend` varchar(20) NOT NULL,       -- Name of batch backend
                 `borrowernumber` int(11),             -- Patron associated with batch
                 `branchcode` varchar(50),             -- Branch associated with batch
+                `statuscode` varchar(20),             -- Status of batch
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `u_illbatches__name` (`name`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        });
+        $dbh->do(q{
+            CREATE TABLE IF NOT EXISTS `illbatch_statuses` (
+                `id` int(11) NOT NULL auto_increment, -- Status ID
+                `name` varchar(100) NOT NULL,         -- Name of status
+                `code` varchar(20) NOT NULL,          -- Unique, immutable code for status
+                `is_system` int(1),                   -- Is this status required for system operation
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `u_illbatchstatuses__code` (`code`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
         });
         $dbh->do(q{
@@ -33,6 +44,11 @@ return {
             ALTER TABLE `illbatches`
                 ADD CONSTRAINT `illbatches_bcfk` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE SET NULL ON UPDATE CASCADE
         });
+        $dbh->do(q{
+            ALTER TABLE `illbatches`
+                ADD CONSTRAINT `illbatches_sfk` FOREIGN KEY (`statuscode`) REFERENCES `illbatch_statuses` (`code`) ON DELETE SET NULL ON UPDATE CASCADE
+        });
+
         say $out "Bug 30719: Add ILL batches completed"
     },
 };
