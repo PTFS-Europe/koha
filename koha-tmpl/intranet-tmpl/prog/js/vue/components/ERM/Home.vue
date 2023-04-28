6615b49628aa346013b9fa01219a17e9631afa55
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2023-04-28 09:16:37 +0000

    test push

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
@@ -1,4 +1,17 @@
-<template></template>
+<template>
+    <div id="toolbar" class="btn-toolbar">
+        <router-link to="#" class="btn btn-default"
+            ><font-awesome-icon icon="plus" />
+            {{ $__("Edit homepage") }}</router-link
+        >
+    </div>
+    <div class="widget-grid">
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+    </div>
+</template>
 
 <script>
 export default {
@@ -9,3 +22,11 @@ export default {
     components: {},
 }
 </script>
+
+<style scoped>
+.widget-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    height: 100%;
+}
+</style>
