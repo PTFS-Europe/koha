153719ff4f4f435d47fd4c111e1359ca5cd017bd
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2023-06-15 14:55:04 +0000

    Bug 34587: Minor fixes
    Replace counter_log.importdate with counter_file.date_uploaded
    Fix month selector display
    Introduce count method in api calls
    Introduce flatpickr
    String fixes
    Add missing id properties
    Replace document with window
    Fix required property on report_types

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/UsageStatisticsDataProviderDetails.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/UsageStatisticsDataProviderDetails.vue
@@ -4,43 +4,43 @@
         <ol>
             <li>
                 <label>{{ $__("Data provider name") }}:</label>
-                <span>
+                <span id="usage_data_provider_name">
                     {{ usage_data_provider.name }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Description") }}:</label>
-                <span>
+                <span id="usage_data_provider_description">
                     {{ usage_data_provider.description }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Status") }}:</label>
-                <span>
+                <span id="harvester_status">
                     {{ usage_data_provider.active ? "Active" : "Inactive" }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Service Type") }}:</label>
-                <span>
+                <span id="usage_data_provider_service_type">
                     {{ usage_data_provider.service_type }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Harvest Start") }}:</label>
-                <span>
+                <span id="usage_data_provider_begin_date">
                     {{ usage_data_provider.begin_date }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Harvest End") }}:</label>
-                <span>
+                <span id="usage_data_provider_end_date">
                     {{ usage_data_provider.end_date }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Report types") }}:</label>
-                <span>
+                <span id="report_type">
                     {{ usage_data_provider.report_types }}
                 </span>
             </li>
@@ -51,43 +51,43 @@
         <ol>
             <li>
                 <label>{{ $__("Service URL") }}:</label>
-                <span>
+                <span id="usage_data_provider_service_url">
                     {{ usage_data_provider.service_url }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Report Release") }}:</label>
-                <span>
+                <span id="usage_data_provider_report_release">
                     {{ usage_data_provider.report_release }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Customer Id") }}:</label>
-                <span>
+                <span id="usage_data_provider_customer_id">
                     {{ usage_data_provider.customer_id }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Requestor Id") }}:</label>
-                <span>
+                <span id="usage_data_provider_requestor_id">
                     {{ usage_data_provider.requestor_id }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("API Key") }}:</label>
-                <span>
+                <span id="usage_data_provider_api_key">
                     {{ usage_data_provider.api_key }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Requestor Name") }}:</label>
-                <span>
+                <span id="usage_data_provider_requestor_name">
                     {{ usage_data_provider.requestor_name }}
                 </span>
             </li>
             <li>
                 <label>{{ $__("Requestor Email") }}:</label>
-                <span>
+                <span id="usage_data_provider_requestor_email">
                     {{ usage_data_provider.requestor_email }}
                 </span>
             </li>
