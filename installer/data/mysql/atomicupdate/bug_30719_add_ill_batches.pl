21df7373480370fa7b4367dc54222b1327a4f1a5
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-06-29 15:29:12 +0000

    Bug 30719: (QA follow-up) Use COMMENT syntax in database files

--- installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
+++ installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
@@ -8,22 +8,22 @@ return {
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
             CREATE TABLE IF NOT EXISTS `illbatches` (
-                `id` int(11) NOT NULL auto_increment, -- Batch ID
-                `name` varchar(100) NOT NULL,         -- Unique name of batch
-                `backend` varchar(20) NOT NULL,       -- Name of batch backend
-                `borrowernumber` int(11),             -- Patron associated with batch
-                `branchcode` varchar(50),             -- Branch associated with batch
-                `statuscode` varchar(20),             -- Status of batch
+                `id` int(11) NOT NULL auto_increment COMMENT "Batch ID",
+                `name` varchar(100) NOT NULL COMMENT "Unique name of batch",
+                `backend` varchar(20) NOT NULL COMMENT "Name of batch backend",
+                `borrowernumber` int(11) COMMENT "Patron associated with batch",
+                `branchcode` varchar(50) COMMENT "Branch associated with batch",
+                `statuscode` varchar(20) COMMENT "Status of batch",
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `u_illbatches__name` (`name`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
         });
         $dbh->do(q{
             CREATE TABLE IF NOT EXISTS `illbatch_statuses` (
-                `id` int(11) NOT NULL auto_increment, -- Status ID
-                `name` varchar(100) NOT NULL,         -- Name of status
-                `code` varchar(20) NOT NULL,          -- Unique, immutable code for status
-                `is_system` int(1),                   -- Is this status required for system operation
+                `id` int(11) NOT NULL auto_increment COMMENT "Status ID",
+                `name` varchar(100) NOT NULL COMMENT "Name of status",
+                `code` varchar(20) NOT NULL COMMENT "Unique, immutable code for status",
+                `is_system` int(1) COMMENT "Is this status required for system operation",
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `u_illbatchstatuses__code` (`code`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
