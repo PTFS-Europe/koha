b00c3b193f37999d6dc8b009d7da9613322bb755
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-06-29 15:32:55 +0000

    Bug 30719: (QA follow-up) Add missing CONSTRAINT entries from kohastructure.sql to the atomicupdate file

--- installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
+++ installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
@@ -7,6 +7,16 @@ return {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
+            CREATE TABLE IF NOT EXISTS `illbatch_statuses` (
+                `id` int(11) NOT NULL auto_increment COMMENT "Status ID",
+                `name` varchar(100) NOT NULL COMMENT "Name of status",
+                `code` varchar(20) NOT NULL COMMENT "Unique, immutable code for status",
+                `is_system` tinyint(1) COMMENT "Is this status required for system operation",
+                PRIMARY KEY (`id`),
+                UNIQUE KEY `u_illbatchstatuses__code` (`code`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        });
+        $dbh->do(q{
             CREATE TABLE IF NOT EXISTS `illbatches` (
                 `id` int(11) NOT NULL auto_increment COMMENT "Batch ID",
                 `name` varchar(100) NOT NULL COMMENT "Unique name of batch",
@@ -15,17 +25,10 @@ return {
                 `branchcode` varchar(50) COMMENT "Branch associated with batch",
                 `statuscode` varchar(20) COMMENT "Status of batch",
                 PRIMARY KEY (`id`),
-                UNIQUE KEY `u_illbatches__name` (`name`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        });
-        $dbh->do(q{
-            CREATE TABLE IF NOT EXISTS `illbatch_statuses` (
-                `id` int(11) NOT NULL auto_increment COMMENT "Status ID",
-                `name` varchar(100) NOT NULL COMMENT "Name of status",
-                `code` varchar(20) NOT NULL COMMENT "Unique, immutable code for status",
-                `is_system` tinyint(1) COMMENT "Is this status required for system operation",
-                PRIMARY KEY (`id`),
-                UNIQUE KEY `u_illbatchstatuses__code` (`code`)
+                UNIQUE KEY `u_illbatches__name` (`name`),
+                CONSTRAINT `illbatches_bnfk` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE,
+                CONSTRAINT `illbatches_bcfk` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE SET NULL ON UPDATE CASCADE,
+                CONSTRAINT `illbatches_sfk` FOREIGN KEY (`statuscode`) REFERENCES `illbatch_statuses` (`code`) ON DELETE SET NULL ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
         });
         unless( column_exists('illrequests', 'batch_id') ) {
