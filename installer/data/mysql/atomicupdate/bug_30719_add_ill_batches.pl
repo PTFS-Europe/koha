d56d97564c6372cb451c3247c3670cc7e396a414
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-07-28 16:03:19 +0000

    Bug 30719: (QA follow-up) Perltidy

--- installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
+++ installer/data/mysql/atomicupdate/bug_30719_add_ill_batches.pl
@@ -1,12 +1,13 @@
 use Modern::Perl;
 
 return {
-    bug_number => "30719",
+    bug_number  => "30719",
     description => "Add ILL batches",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
-        $dbh->do(q{
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+        $dbh->do(
+            q{
             CREATE TABLE IF NOT EXISTS `illbatch_statuses` (
                 `id` int(11) NOT NULL auto_increment COMMENT "Status ID",
                 `name` varchar(100) NOT NULL COMMENT "Name of status",
@@ -15,8 +16,10 @@ return {
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `u_illbatchstatuses__code` (`code`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        });
-        $dbh->do(q{
+        }
+        );
+        $dbh->do(
+            q{
             CREATE TABLE IF NOT EXISTS `illbatches` (
                 `id` int(11) NOT NULL auto_increment COMMENT "Batch ID",
                 `name` varchar(100) NOT NULL COMMENT "Unique name of batch",
@@ -30,40 +33,51 @@ return {
                 CONSTRAINT `illbatches_bcfk` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE SET NULL ON UPDATE CASCADE,
                 CONSTRAINT `illbatches_sfk` FOREIGN KEY (`statuscode`) REFERENCES `illbatch_statuses` (`code`) ON DELETE SET NULL ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        });
-        unless( column_exists('illrequests', 'batch_id') ) {
-            $dbh->do(q{
+        }
+        );
+        unless ( column_exists( 'illrequests', 'batch_id' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `illrequests`
                     ADD COLUMN `batch_id` int(11) AFTER backend -- Optional ID of batch that this request belongs to
-            });
+            }
+            );
         }
 
-        unless ( foreign_key_exists( 'illrequests', 'illrequests_ibfk' ) ){
-            $dbh->do(q{
+        unless ( foreign_key_exists( 'illrequests', 'illrequests_ibfk' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `illrequests`
                     ADD CONSTRAINT `illrequests_ibfk` FOREIGN KEY (`batch_id`) REFERENCES `illbatches` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-            });
+            }
+            );
         }
 
-        unless ( foreign_key_exists( 'illbatches', 'illbatches_bnfk' ) ){
-            $dbh->do(q{
+        unless ( foreign_key_exists( 'illbatches', 'illbatches_bnfk' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `illbatches`
                     ADD CONSTRAINT `illbatches_bnfk` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE
-            });
+            }
+            );
         }
 
-        unless ( foreign_key_exists( 'illbatches', 'illbatches_bcfk' ) ){
-            $dbh->do(q{
+        unless ( foreign_key_exists( 'illbatches', 'illbatches_bcfk' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `illbatches`
                     ADD CONSTRAINT `illbatches_bcfk` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE SET NULL ON UPDATE CASCADE
-            });
+            }
+            );
         }
 
-        unless ( foreign_key_exists( 'illbatches', 'illbatches_sfk' ) ){
-            $dbh->do(q{
+        unless ( foreign_key_exists( 'illbatches', 'illbatches_sfk' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `illbatches`
                     ADD CONSTRAINT `illbatches_sfk` FOREIGN KEY (`statuscode`) REFERENCES `illbatch_statuses` (`code`) ON DELETE SET NULL ON UPDATE CASCADE
-            });
+            }
+            );
         }
 
         # Get any existing NEW batch status
@@ -76,9 +90,11 @@ return {
         if ($new_status) {
             say $out "Bug 30719: NEW ILL batch status found. Update has already been run.";
         } else {
-            $dbh->do(qq{
+            $dbh->do(
+                qq{
             INSERT INTO illbatch_statuses ( name, code, is_system ) VALUES ('New', 'NEW', '1')
-            });
+            }
+            );
             say $out "Bug 30719: Added NEW ILL batch status";
         }
 
@@ -92,9 +108,11 @@ return {
         if ($in_progress_status) {
             say $out "Bug 30719: IN_PROGRESS ILL batch status found. Update has already been run.";
         } else {
-            $dbh->do(qq{
+            $dbh->do(
+                qq{
             INSERT INTO illbatch_statuses( name, code, is_system ) VALUES( 'In progress', 'IN_PROGRESS', '1' )
-            });
+            }
+            );
             say $out "Bug 30719: Added IN_PROGRESS ILL batch status";
         }
 
@@ -108,9 +126,11 @@ return {
         if ($completed_status) {
             say $out "Bug 30719: COMPLETED ILL batch status found. Update has already been run.";
         } else {
-            $dbh->do(qq{
+            $dbh->do(
+                qq{
             INSERT INTO illbatch_statuses( name, code, is_system ) VALUES( 'Completed', 'COMPLETED', '1' )
-            });
+            }
+            );
             say $out "Bug 30719: Added COMPLETED ILL batch status";
         }
 
@@ -124,12 +144,14 @@ return {
         if ($unknown_status) {
             say $out "Bug 30719: UNKNOWN ILL batch status found. Update has already been run.";
         } else {
-            $dbh->do(qq{
+            $dbh->do(
+                qq{
             INSERT INTO illbatch_statuses( name, code, is_system ) VALUES( 'Unknown', 'UNKNOWN', '1' )
-            });
+            }
+            );
             say $out "Bug 30719: Added UNKNOWN ILL batch status";
         }
 
-        say $out "Bug 30719: Add ILL batches completed"
+        say $out "Bug 30719: Add ILL batches completed";
     },
 };
