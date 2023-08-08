ad74d12cb57696c8bbbd427aaf0363a1a6129a63
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-08-08 11:15:12 +0000

    Bug 34587: Accept date inputs upon confirming 'run now' button
    (cherry picked from commit 354e10a6e498745250e036e957181257bd2d2ea3)
    (cherry picked from commit 083e61c6da413a0db772515a0a8a68a9ad5c9bec)

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/erm-api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/erm-api-client.js
@@ -250,20 +250,20 @@ export class ERMAPIClient extends HttpClient {
 
     get usage_data_providers() {
         return {
-            get: (id) =>
+            get: id =>
                 this.get({
                     endpoint: "usage_data_providers/" + id,
                 }),
-            getAll: (query) =>
+            getAll: query =>
                 this.get({
                     endpoint: "usage_data_providers",
-                    query
+                    query,
                 }),
-            delete: (id) =>
+            delete: id =>
                 this.delete({
                     endpoint: "usage_data_providers/" + id,
                 }),
-            create: (usage_data_provider) =>
+            create: usage_data_provider =>
                 this.post({
                     endpoint: "usage_data_providers",
                     body: usage_data_provider,
@@ -273,11 +273,12 @@ export class ERMAPIClient extends HttpClient {
                     endpoint: "usage_data_providers/" + id,
                     body: usage_data_provider,
                 }),
-            run: (id, begin_date, end_date) =>
-                this.get({
-                    endpoint: "usage_data_providers/" + id + "/run?begin_date="+ begin_date + "&end_date=" + end_date,
+            run: (id, body) =>
+                this.post({
+                    endpoint: "usage_data_providers/" + id + "/run",
+                    body: body,
                 }),
-            test: (id) =>
+            test: id =>
                 this.get({
                     endpoint: "usage_data_providers/" + id + "/test_connection",
                 }),
@@ -296,26 +297,24 @@ export class ERMAPIClient extends HttpClient {
 
     get usage_titles() {
         return {
-            get: (id) =>
+            get: id =>
                 this.get({
                     endpoint: "usage_titles/" + id,
                     headers: {
-                        "x-koha-embed":
-                            "usage_mus",
+                        "x-koha-embed": "usage_mus",
                     },
                 }),
-            getAll: (query) =>
+            getAll: query =>
                 this.getAll({
                     endpoint: "usage_titles",
-                    query
+                    query,
                 }),
             getReport: (query, embed) =>
                 this.get({
                     endpoint: "usage_titles/report",
                     query,
                     headers: {
-                        "x-koha-embed":
-                            `${embed}`,
+                        "x-koha-embed": `${embed}`,
                     },
                 }),
             count: (query = {}) =>
@@ -333,21 +332,20 @@ export class ERMAPIClient extends HttpClient {
 
     get counter_files() {
         return {
-            getAll: (query) =>
+            getAll: query =>
                 this.get({
                     endpoint: "counter_files",
                     query,
                     headers: {
-                        "x-koha-embed":
-                            "counter_logs",
-                    }
+                        "x-koha-embed": "counter_logs",
+                    },
                 }),
-            create: (counter_file) =>
+            create: counter_file =>
                 this.post({
                     endpoint: "counter_files",
                     body: counter_file,
                 }),
-            delete: (id) =>
+            delete: id =>
                 this.delete({
                     endpoint: "counter_files/" + id,
                 }),
@@ -366,17 +364,17 @@ export class ERMAPIClient extends HttpClient {
 
     get default_usage_reports() {
         return {
-            getAll: (query) =>
+            getAll: query =>
                 this.get({
                     endpoint: "default_usage_reports",
                     query,
                 }),
-            create: (default_usage_report) =>
+            create: default_usage_report =>
                 this.post({
                     endpoint: "default_usage_reports",
                     body: default_usage_report,
                 }),
-            delete: (id) =>
+            delete: id =>
                 this.delete({
                     endpoint: "default_usage_reports/" + id,
                 }),
