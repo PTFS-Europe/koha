b29f275d850be41e07ce9422a4928d0602498b4a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-02-07 15:21:04 +0000

    Bug 35977: Set holddatefrom default to today
    This patch sets the holddatefrom date to today by default on the opac.

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -35,6 +35,7 @@ use C4::Overdues;
 use Koha::AuthorisedValues;
 use Koha::Biblios;
 use Koha::CirculationRules;
+use Koha::DateUtils qw( dt_from_string );
 use Koha::Items;
 use Koha::ItemTypes;
 use Koha::Checkouts;
@@ -623,6 +624,7 @@ if (
     ) {
     $template->param(
 	    reserve_in_future         => 1,
+        today                     => dt_from_string,
     );
 }
 
