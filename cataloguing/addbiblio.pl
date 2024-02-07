fb0f715231579fb13114f1c77e6459ecb4165edc
Tomas Cohen Arazi <tomascohen@theke.io>
2024-02-07 11:06:32 -0300

    Bug 31791: Limit editing locked records
    Sponsored-by: ByWater Solutions
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -537,12 +537,21 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     }
 );
 
+my $logged_in_patron = Koha::Patrons->find($loggedinuser);
 my $biblio;
-if ($biblionumber){
+
+if ($biblionumber) {
+
     $biblio = Koha::Biblios->find($biblionumber);
-    unless ( $biblio ) {
+
+    unless ($biblio) {
         $biblionumber = undef;
         $template->param( bib_doesnt_exist => 1 );
+    }
+
+    unless ( $biblio->can_be_edited($logged_in_patron) ) {
+        print $input->redirect("/cgi-bin/koha/errors/403.pl");    # escape early
+        exit;
     }
 }
 
@@ -645,15 +654,14 @@ if ($biblionumber) {
     $sth->execute($biblionumber);
     ($biblioitemnumber) = $sth->fetchrow;
     if (C4::Context->preference('MARCOverlayRules')) {
-        my $member = Koha::Patrons->find($loggedinuser);
         $record = ApplyMarcOverlayRules(
             {
                 biblionumber    => $biblionumber,
                 record          => $record,
                 overlay_context =>  {
                         source       => $z3950 ? 'z3950' : 'intranet',
-                        categorycode => $member->categorycode,
-                        userid       => $member->userid
+                        categorycode => $logged_in_patron->categorycode,
+                        userid       => $logged_in_patron->userid,
                 }
             }
         );
@@ -679,7 +687,6 @@ if ( $op eq "cud-addbiblio" ) {
     if ( !$duplicatebiblionumber or $confirm_not_duplicate ) {
         my $oldbibitemnum;
         if ( $is_a_modif ) {
-            my $member = Koha::Patrons->find($loggedinuser);
             ModBiblio(
                 $record,
                 $biblionumber,
@@ -687,8 +694,8 @@ if ( $op eq "cud-addbiblio" ) {
                 {
                     overlay_context => {
                         source       => $z3950 ? 'z3950' : 'intranet',
-                        categorycode => $member->categorycode,
-                        userid       => $member->userid
+                        categorycode => $logged_in_patron->categorycode,
+                        userid       => $logged_in_patron->userid,
                     }
                 }
             );
