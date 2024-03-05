e459f767465e370e7f0103946fb3a3a3a605b2ae
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-03-05 17:09:28 +0000

    Bug 36197: Only check for can_place_ill_in_opac if a user is logged in
    Test this, it looks wrong.
    can_place_ill_in_opac should be ignore for unauth requests

--- opac/opac-illrequests.pl
+++ opac/opac-illrequests.pl
@@ -79,7 +79,7 @@ if ( $illrequest_id = $params->{illrequest_id} ) {
     }
 }
 
-if ( ( $op eq 'cud-create' || $op eq 'cancreq' || $op eq 'cud-update' ) && !$patron->_result->categorycode->can_place_ill_in_opac ) {
+if ( ( $op eq 'cud-create' || $op eq 'cancreq' || $op eq 'cud-update' ) && ($patron && !$patron->_result->categorycode->can_place_ill_in_opac) ) {
     print $query->redirect('/cgi-bin/koha/errors/403.pl');
     exit;
 }
@@ -204,7 +204,7 @@ if ( $op eq 'list' ) {
 
 $template->param(
     unauthenticated_ill => C4::Context->preference('ILLOpacUnauthenticatedRequest'),
-    can_place_ill_in_opac => $patron->_result->categorycode->can_place_ill_in_opac,
+    can_place_ill_in_opac => ( $patron && $patron->_result->categorycode->can_place_ill_in_opac ),
     message               => $params->{message},
     illrequestsview       => 1,
     op                    => $op
