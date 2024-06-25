20b276d3e57b5931066412b905b91f304acfe428
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-06-25 13:15:40 +0100

    Bug 31097: Downgrade atomic update to old style for 23.11.x backport

--- installer/data/mysql/atomicupdate/bug_31097.pl
+++ installer/data/mysql/atomicupdate/bug_31097.pl
@@ -1,5 +1,4 @@
 use Modern::Perl;
-use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "31097",
@@ -10,9 +9,6 @@ return {
 
         $dbh->do(q{ UPDATE restriction_types SET display_text = "Manual restriction" WHERE display_text = "Manual"});
 
-        say_info(
-            $out,
-            "Updated patron restriction types display for MANUAL restrictions from 'Manual' to 'Manual restriction'"
-        );
+        say $out "Updated patron restriction types display for MANUAL restrictions from 'Manual' to 'Manual restriction'";
     },
 };
