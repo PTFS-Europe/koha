c1acb047f3e183c4418f4068d114eb9903b441b3
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-07-09 14:30:40 +0000

    Bug 38201: [PoC] DRY ToolbarButton
    This is only a PoC applied to Agreements for discussion.
    This is a continuation of bug 37274.
    Bug 37274 applied the ToolbarButton to missing components but it was mostly replacing WET for WET.
    
    This is DRYer:
    - All mark-up related to toolbar buttons is now written once (css class, icons, elements)
    - All labels are now written once (except 'Add' for translatability)

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
@@ -1,12 +1,22 @@
 <template>
-    <router-link :to="to" class="btn btn-default"
-        ><font-awesome-icon v-if="icon" :icon="icon" /> {{ title }}</router-link
+    <a v-if="type === 'add'" @click="click_add" class="btn btn-default"
+        ><font-awesome-icon icon="plus" /> {{ title }}</a
+    >
+    <a v-if="type === 'delete'" @click="click_delete" class="btn btn-default"
+        ><font-awesome-icon icon="trash" /> {{ $__("Delete") }}</a
+    >
+    <a v-if="type === 'edit'" @click="click_edit" class="btn btn-default"
+        ><font-awesome-icon icon="pencil" /> {{ $__("Edit") }}</a
     >
 </template>
 
 <script>
 export default {
     props: {
+        type: {
+            type: String,
+            required: true,
+        },
         to: {
             type: [String, Object],
         },
@@ -18,6 +28,18 @@ export default {
             type: String,
         },
     },
+    methods: {
+        click_add() {
+            this.$emit("add-clicked");
+        },
+        click_delete() {
+            this.$emit("delete-clicked");
+        },
+        click_edit() {
+            this.$emit("edit-clicked");
+        },
+    },
+    emits: ["add-clicked", "delete-clicked", "edit-clicked"],
     name: "Toolbar",
 };
 </script>
