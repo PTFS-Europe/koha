bb9f99f439ce666b28e2d604f9cc4fe47807b070
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-07-09 14:30:40 +0000

    Bug 37274: [PoC] DRY ToolbarButton
    Applied to Agreements example

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
@@ -1,12 +1,22 @@
 <template>
-    <router-link :to="to" :class="class"
-        ><font-awesome-icon v-if="icon" :icon="icon" /> {{ title }}</router-link
+    <a v-if="type === 'add'" @click="click_add" :class="class"
+        ><font-awesome-icon icon="plus" /> {{ title }}</a
+    >
+    <a v-if="type === 'delete'" @click="click_delete" :class="class"
+        ><font-awesome-icon icon="trash" /> {{ $__("Delete") }}</a
+    >
+    <a v-if="type === 'edit'" @click="click_edit" :class="class"
+        ><font-awesome-icon icon="pencil" /> {{ $__("Edit") }}</a
     >
 </template>
 
 <script>
 export default {
     props: {
+        type: {
+            type: String,
+            required: true,
+        },
         to: {
             type: [String, Object],
         },
@@ -22,6 +32,18 @@ export default {
             type: String,
         },
     },
+    methods: {
+        click_add() {
+            this.$emit("add-clicked")
+        },
+        click_delete() {
+            this.$emit("delete-clicked")
+        },
+        click_edit() {
+            this.$emit("edit-clicked")
+        },
+    },
+    emits: ["add-clicked", "delete-clicked", "edit-clicked"],
     name: "Toolbar",
 }
 </script>
