8090920de198eafd5b688d4445c311c2e9d3eae4
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-07-10 14:27:37 +0200

    Bug 38201: Rename 'type' with 'action'
    'type' is too generic.
    Also move to else-if and add a else block to ease debugging in case an
    unsupported action is passed

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
@@ -1,28 +1,29 @@
 <template>
     <a
-        v-if="type === 'add'"
+        v-if="action === 'add'"
         @click="$emit('addResource')"
         class="btn btn-default"
         ><font-awesome-icon icon="plus" /> {{ title }}</a
     >
     <a
-        v-if="type === 'delete'"
+        v-else-if="action === 'delete'"
         @click="$emit('deleteResource')"
         class="btn btn-default"
         ><font-awesome-icon icon="trash" /> {{ $__("Delete") }}</a
     >
     <a
-        v-if="type === 'edit'"
+        v-else-if="action === 'edit'"
         @click="$emit('editResource')"
         class="btn btn-default"
         ><font-awesome-icon icon="pencil" /> {{ $__("Edit") }}</a
     >
+    <span v-else>{{ $__("Unknown action %s").format(action) }}</span>
 </template>
 
 <script>
 export default {
     props: {
-        type: {
+        action: {
             type: String,
             required: true,
         },
