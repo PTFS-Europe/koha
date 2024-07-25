fc30e5fa59287244274090489519dba5d4025e99
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-07-25 10:27:19 +0000

    Bug 37389: Move all extended_attributes query logic into Koha/Objects.pm
    Generate the extended attributes dynamic accessors anytime they're needed, not just when being queried from the REST API

--- Koha/REST/Plugin/Objects.pm
+++ Koha/REST/Plugin/Objects.pm
@@ -287,15 +287,6 @@ controller, and thus shouldn't be called twice in it.
             $c->stash('koha.pagination.base_total'   => $result_set->count);
             $c->stash('koha.pagination.query_params' => $args);
 
-            # Check and handle related metadata table joins
-            $c->dbic_extended_attributes_join(
-                {
-                    attributes      => $attributes,
-                    filtered_params => $filtered_params,
-                    result_set      => $result_set
-                }
-            );
-
             # Generate the resultset
             my $objects_rs = $result_set->search( $filtered_params, $attributes );
             # Stash the page total if requires, total otherwise
