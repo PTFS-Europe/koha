5f165fba2b1871f3f3300d9e84e470ada8928458
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-08-13 14:17:34 +0000

    Bug 10190: Add a library API client
    This patch adds a library api client to the vue ecosystem for later use
    in the Circulation triggers page.
    
    Sponsored-by: Glasgow Colleges Library Group
    (cherry picked from commit 416cc239be7053a24086ac6e1aa32ff7a352a104)

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
@@ -1,19 +1,23 @@
 import ERMAPIClient from "./erm-api-client";
 import PatronAPIClient from "./patron-api-client";
+import LibraryAPIClient from "./library-api-client";
 import AcquisitionAPIClient from "./acquisition-api-client";
 import AdditionalFieldsAPIClient from "./additional-fields-api-client";
 import AVAPIClient from "./authorised-values-api-client";
 import ItemAPIClient from "./item-api-client";
 import SysprefAPIClient from "./system-preferences-api-client";
 import PreservationAPIClient from "./preservation-api-client";
+import CircRuleAPIClient from "./circulation-rules-api-client";
 
 export const APIClient = {
     erm: new ERMAPIClient(),
     patron: new PatronAPIClient(),
+    library: new LibraryAPIClient(),
     acquisition: new AcquisitionAPIClient(),
     additional_fields: new AdditionalFieldsAPIClient(),
     authorised_values: new AVAPIClient(),
     item: new ItemAPIClient(),
     sysprefs: new SysprefAPIClient(),
     preservation: new PreservationAPIClient(),
+    circRule: new CircRuleAPIClient(),
 };
