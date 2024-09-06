3f98f454a3cf6fc9f55a83ff3738b7fffc77944f
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-09-06 12:34:08 +0100

    Bug 10190: Restore 'edit' from 'Add/Edit' Modal
    Sponsored-by: Glasgow Colleges Library Group <https://library.cityofglasgowcollege.ac.uk>
    Signed-off-by: George Harkins <George.Harkins@cityofglasgowcollege.ac.uk>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Admin/CirculationTriggers/TriggersTable.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Admin/CirculationTriggers/TriggersTable.vue
@@ -30,7 +30,7 @@
                 <th>
                     {{ $__("Restricts checkouts") }}
                 </th>
-                <th v-if="!modal">
+                <th>
                     {{ $__("Actions") }}
                 </th>
             </thead>
@@ -155,7 +155,7 @@
                         </span>
                     </td>
 
-                    <td v-if="!modal" class="actions">
+                    <td class="actions">
                         <router-link
                             :to="{
                                 name: 'CirculationTriggersFormEdit',
@@ -164,7 +164,7 @@
                                     item_type_id: rule.context.item_type_id,
                                     patron_category_id:
                                         rule.context.patron_category_id,
-                                    triggerNumber,
+                                    triggerNumber: modal ? i + 1 : triggerNumber,
                                 },
                             }"
                             class="btn btn-default btn-xs"
