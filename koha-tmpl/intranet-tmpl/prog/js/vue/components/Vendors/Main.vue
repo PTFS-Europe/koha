ca21c8423fe27b31e6937de9f12211277e983cab
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-09-11 09:11:32 +0000

    Bug 38010: Add vue app and list component
    This patch adds the base vue app as well as the component to list vendors

---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/Main.vue
@@ -0,0 +1,103 @@
+<template>
+    <div v-if="initialized">
+        <div id="sub-header">
+            <Breadcrumbs />
+            <Help />
+        </div>
+        <div class="main container-fluid">
+            <div class="row">
+                <div class="col-md-10 order-md-2 order-sm-1">
+                    <main>
+                        <Dialog />
+                        <router-view />
+                    </main>
+                </div>
+
+                <div class="col-md-2 order-sm-2 order-md-1">
+                    <LeftMenu :title="$__('Vendors')"></LeftMenu>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main container-fluid" v-else>
+        <Dialog />
+    </div>
+</template>
+
+<script>
+import { inject } from "vue"
+import Breadcrumbs from "../Breadcrumbs.vue"
+import Help from "../Help.vue"
+import LeftMenu from "../LeftMenu.vue"
+import Dialog from "../Dialog.vue"
+import "vue-select/dist/vue-select.css"
+import { storeToRefs } from "pinia"
+
+export default {
+    setup() {
+        const vendorStore = inject("vendorStore")
+
+        const AVStore = inject("AVStore")
+
+        const mainStore = inject("mainStore")
+
+        const { loading, loaded, setError } = mainStore
+
+        return {
+            vendorStore,
+            AVStore,
+            setError,
+            loading,
+            loaded,
+        }
+    },
+    data() {
+        return {
+            initialized: true,
+        }
+    },
+    methods: {},
+    components: {
+        Breadcrumbs,
+        Dialog,
+        Help,
+        LeftMenu,
+    },
+}
+</script>
+
+<style>
+#menu ul ul,
+#navmenulist ul ul {
+    padding-left: 2em;
+    font-size: 100%;
+}
+
+form .v-select {
+    display: inline-block;
+    background-color: white;
+    width: 30%;
+}
+
+.v-select,
+input:not([type="submit"]):not([type="search"]):not([type="button"]):not([type="checkbox"]):not([type="radio"]),
+textarea {
+    border-color: rgba(60, 60, 60, 0.26);
+    border-width: 1px;
+    border-radius: 4px;
+    min-width: 30%;
+}
+.flatpickr-input {
+    width: 30%;
+}
+#navmenulist ul li a.current.disabled {
+    background-color: inherit;
+    border-left: 5px solid #e6e6e6;
+    color: #000;
+}
+#navmenulist ul li a.disabled {
+    color: #666;
+    pointer-events: none;
+    font-weight: 700;
+}
+</style>
