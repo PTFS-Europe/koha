2ac45eb9b7f2a44d45e1721a592e212b9a15c790
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-09-16 15:03:57 +0000

    Plugin store: Plugin store table updates
    Install is now only available if plugin has not yet been installed and there are adequate releases available.
    Update now shows if a more recent compatible version exists on the plugin store
    Visual labels are now shown for greater visual clarity

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Plugin-store/StorePluginsList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Plugin-store/StorePluginsList.vue
@@ -1,10 +1,18 @@
 <template>
     <div id="store_plugins_list">
+        <div class="btn-toolbar" id="toolbar">
+            <a
+                href="/cgi-bin/koha/plugins/plugins-home.pl"
+                class="btn btn-default"
+                ><i class="fa-solid fa-eye"></i> View installed plugins</a
+            >
+        </div>
         <div class="page-section">
             <KohaTable
                 ref="table"
                 v-bind="tableOptions"
                 @install="doInstall"
+                @update="doUpdate"
             ></KohaTable>
         </div>
     </div>
@@ -23,14 +31,16 @@ export default {
             inject("mainStore")
 
         return {
+            koha_version,
+            installed_plugins,
             setConfirmationDialog,
             setMessage,
             setWarning,
         }
     },
     data: function () {
+        let component = this
         return {
-            initialized: false,
             tableOptions: {
                 columns: [
                     {
@@ -64,6 +74,47 @@ export default {
                         searchable: false,
                         orderable: false,
                     },
+                    {
+                        title: "Latest release",
+                        data: "releases",
+                        searchable: false,
+                        orderable: false,
+                        render: function (data, type, row, meta) {
+                            if (component.pluginHasNoReleases(row)) {
+                                return '<span class="badge text-bg-warning"> No releases available for this Koha version!</span>'
+                            }
+                            if (!data || data.length === 0) return "N/A"
+                            let most_recent_release =
+                                component.getMostRecentRelease(row)
+                            return most_recent_release.version
+                        },
+                    },
+                    {
+                        title: "Installed release",
+                        data: "releases",
+                        searchable: false,
+                        orderable: false,
+                        render: function (data, type, row, meta) {
+                            let installed = component.installed_plugins.find(
+                                ip => ip.class === row.class_name
+                            )
+                            if (!installed) return "N/A"
+                            let returnHTML = installed.metadata.version
+                            if (
+                                !component.installedPluginVersionIsLatest(
+                                    installed,
+                                    row
+                                )
+                            ) {
+                                returnHTML +=
+                                    ' <span class="badge text-bg-warning"> Update available</span>'
+                            } else {
+                                returnHTML +=
+                                    ' <span class="badge text-bg-success"> Up to date!</span>'
+                            }
+                            return returnHTML
+                        },
+                    },
                 ],
                 url: () => this.table_url(),
                 // FIXME: Use table settings instead, something like:
@@ -94,6 +145,18 @@ export default {
                             cannot_be_toggled: 0,
                             columnname: "author",
                         },
+                        {
+                            is_hidden: 0,
+                            cannot_be_modified: 0,
+                            cannot_be_toggled: 0,
+                            columnname: "latest_release",
+                        },
+                        {
+                            is_hidden: 0,
+                            cannot_be_modified: 0,
+                            cannot_be_toggled: 0,
+                            columnname: "installed_release",
+                        },
                     ],
                     default_display_length: null,
                     module: "store-plugins",
@@ -101,16 +164,7 @@ export default {
                     table: "store-plugins",
                     page: "store-plugins",
                 },
-                actions: {
-                    "-1": [
-                        {
-                            install: {
-                                text: this.$__("Install"),
-                                icon: "fa fa-download",
-                            },
-                        },
-                    ],
-                },
+                actions: this.getTableActions(),
                 default_filters: {},
             },
             before_route_entered: false,
@@ -118,12 +172,59 @@ export default {
         }
     },
     methods: {
+        getTableActions: function () {
+            let component = this
+            //FIXME: Can't figure out how to get kohatable to wait for installed_plugins before rendering
+            return {
+                "-1": [
+                    {
+                        install: {
+                            text: this.$__("Install"),
+                            icon: "fa fa-download",
+                            should_display: function (row) {
+                                let installed =
+                                    component.installed_plugins.find(
+                                        ip => ip.class === row.class_name
+                                    )
+                                let no_releases =
+                                    component.pluginHasNoReleases(row)
+
+                                return installed || no_releases ? 0 : 1
+                            },
+                        },
+                    },
+                    {
+                        update: {
+                            text: this.$__("Update"),
+                            icon: "fa fa-refresh",
+                            should_display: function (row) {
+                                let installed =
+                                    component.installed_plugins.find(
+                                        ip => ip.class === row.class_name
+                                    )
+                                if (!installed) return 0
+                                if (
+                                    !component.installedPluginVersionIsLatest(
+                                        installed,
+                                        row
+                                    )
+                                ) {
+                                    return 1
+                                }
+                                return 0
+                            },
+                        },
+                    },
+                ],
+            }
+        },
         doInstall: function (plugin, dt, event) {
-            //FIXME: This is installing the first release, not checking any koha version or anything
+            //FIXME: This is installing the most recent release, not checking any koha version or anything
+            let most_recent_release = this.getMostRecentRelease(plugin)
             const client = APIClient.plugin_store
             client.plugins
                 .create({
-                    kpz_url: plugin.releases[0].kpz_url,
+                    kpz_url: most_recent_release.kpz_url,
                 })
                 .then(
                     res => {
@@ -136,10 +237,63 @@ export default {
                     error => {}
                 )
         },
+        doUpdate: function (plugin, dt, event) {
+            //FIXME: This is installing the first release, not checking any koha version or anything
+            let most_recent_release = this.getMostRecentRelease(plugin)
+            const client = APIClient.plugin_store
+            client.plugins
+                .create({
+                    kpz_url: most_recent_release.kpz_url,
+                })
+                .then(
+                    res => {
+                        this.setMessage(
+                            this.$__(
+                                'Plugin has been updated. <a href="/cgi-bin/koha/plugins/plugins-home.pl">Manage plugins</a>'
+                            )
+                        )
+                    },
+                    error => {}
+                )
+        },
+        installedPluginVersionIsLatest: function (
+            installed_plugin,
+            candidate_plugin
+        ) {
+            if (!installed_plugin) {
+                return 0
+            }
+            let most_recent_release =
+                this.getMostRecentRelease(candidate_plugin)
+            if (
+                installed_plugin.metadata.version ===
+                most_recent_release.version
+            ) {
+                return 1
+            }
+            return 0
+        },
+        getMostRecentRelease: function (candidate_plugin) {
+            return candidate_plugin.releases.reduce(
+                (most_recent, release) => {
+                    const most_recent_date = new Date(most_recent.date_released)
+                    const release_date = new Date(release.date_released)
+                    return release_date > most_recent_date
+                        ? release
+                        : most_recent
+                },
+                { date_released: 0 }
+            )
+        },
+        pluginHasNoReleases: function (plugin) {
+            return !plugin.releases || plugin.releases.length === 0
+        },
         table_url: function () {
             // FIXME: This is hardcoded
-            return "http://localhost:3000/api/plugins"
-            this.initialized = true
+            return (
+                "http://localhost:3000/api/plugins?koha_version_release=" +
+                this.koha_version.release
+            )
         },
     },
     components: { Toolbar, ToolbarButton, KohaTable },
