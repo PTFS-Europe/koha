bff3617f03feb4e0c2f08975e21f342b8760838e
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-09-19 14:43:25 +0000

    Bug 38010: Add the form to add a new vendor

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/Main.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/Main.vue
@@ -102,11 +102,18 @@ export default {
                 }
             })
             this.loaded()
+            this.userPermissions = userPermissions
+            this.vendorStore.currencies = currencies
+            this.vendorStore.gstValues = gstValues.map(gv => {
+                return {
+                    label: `${(gv.option * 100).toFixed(2)}%`,
+                    value: gv.option,
+                }
+            })
             this.initialized = true
         })
     },
     data() {
-        this.userPermissions = userPermissions
         return {
             initialized: false,
         }
