0cc6f3736806d3f844bca8465a239223fa0b6cc6
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-09-19 14:43:25 +0000

    Bug 38010: Add the form to add a new vendor

--- koha-tmpl/intranet-tmpl/prog/js/vue/stores/vendors.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/stores/vendors.js
@@ -3,6 +3,8 @@ import { defineStore } from "pinia";
 export const useVendorStore = defineStore("vendors", {
     state: () => ({
         vendors: [],
+        currencies: [],
+        gstValues: [],
         authorisedValues: {
             vendor_types: "VENDOR_TYPE",
             vendor_interface_types: "VENDOR_INTERFACE_TYPE",
