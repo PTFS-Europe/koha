fd152cc9737dd52283cafc12f3a5f650e3e8252f
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-09-19 10:05:56 +0000

    Bug 38010: Rename routes to acquisitions
    This patch renames the app from vendors to acquisitions to allow further acquisitions pages to be migrated

--- rspack.config.js
+++ rspack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/preservation.ts",
         "admin/record_sources":
             "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/admin/record_sources.ts",
-        vendors: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/vendors.ts",
+        acquisitions: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/acquisitions.ts",
     },
     output: {
         filename: "[name].js",
