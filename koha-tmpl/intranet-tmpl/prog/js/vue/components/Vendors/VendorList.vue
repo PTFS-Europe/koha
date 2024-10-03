b09ac8e4c05a8916cc154ec45cf58f4b5b7cdc6a
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-10-03 13:13:45 +0000

    Bug 38010: Allow embedding for baskets table

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorList.vue
@@ -78,6 +78,7 @@ export default {
                     }),
                 },
                 actions: {
+                    0: ["show"],
                     "-1": [
                         "edit",
                         {
