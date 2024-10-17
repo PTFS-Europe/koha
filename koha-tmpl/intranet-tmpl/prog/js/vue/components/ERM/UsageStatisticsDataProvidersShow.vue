43cee237f0274b5c7a710ad61ad9f8dd9b36d087
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-10-17 15:15:53 +0000

    Bug 37301: Fix: URL param needs to match attr_id
    url param needs to match attr_id
    usage statistics is non-standard when compared to agreements+licenses etc.
    using usage_data_provider_id but resource id field is erm_usage_data_provider_id.
    This patch is fixing that.

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/UsageStatisticsDataProvidersShow.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/UsageStatisticsDataProvidersShow.vue
@@ -171,7 +171,7 @@ export default {
     },
     beforeRouteEnter(to, from, next) {
         next(vm => {
-            vm.getUsageDataProvider(to.params.usage_data_provider_id)
+            vm.getUsageDataProvider(to.params.erm_usage_data_provider_id)
         })
     },
     methods: {
