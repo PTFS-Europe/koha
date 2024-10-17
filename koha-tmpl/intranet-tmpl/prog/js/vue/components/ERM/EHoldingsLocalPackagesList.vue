13376faf266f3e68b0350317c104ca1e00f6cb3c
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-10-17 11:48:23 +0000

    Bug 37301: Apply changes to eHoldings local packages

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/EHoldingsLocalPackagesList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/EHoldingsLocalPackagesList.vue
@@ -4,8 +4,8 @@
         <div v-else id="packages_list">
             <Toolbar>
                 <ToolbarButton
-                    :to="{ name: 'EHoldingsLocalPackagesFormAdd' }"
-                    icon="plus"
+                    action="add"
+                    @go-to-add-resource="goToResourceAdd"
                     :title="$__('New package')"
                 />
             </Toolbar>
@@ -17,9 +17,9 @@
                 <KohaTable
                     ref="table"
                     v-bind="tableOptions"
-                    @show="doShow"
-                    @edit="doEdit"
-                    @delete="doDelete"
+                    @show="goToResourceShow"
+                    @edit="goToResourceEdit"
+                    @delete="doResourceDelete"
                 ></KohaTable>
             </div>
             <div v-else class="alert alert-info">
@@ -36,16 +36,16 @@ import { inject, ref, reactive } from "vue"
 import { storeToRefs } from "pinia"
 import { APIClient } from "../../fetch/api-client.js"
 import KohaTable from "../KohaTable.vue"
+import EHoldingsLocalPackageResource from "./EHoldingsLocalPackageResource.vue"
 
 export default {
+    extends: EHoldingsLocalPackageResource,
     setup() {
         const vendorStore = inject("vendorStore")
         const { vendors } = storeToRefs(vendorStore)
 
         const AVStore = inject("AVStore")
         const { get_lib_from_av, map_av_dt_filter } = AVStore
-
-        const { setConfirmationDialog, setMessage } = inject("mainStore")
 
         const table = ref()
         const filters = reactive({
@@ -54,13 +54,12 @@ export default {
         })
 
         return {
+            ...EHoldingsLocalPackageResource.setup(),
             vendors,
             get_lib_from_av,
             map_av_dt_filter,
             table,
             filters,
-            setConfirmationDialog,
-            setMessage,
             escape_str,
             eholdings_packages_table_settings,
         }
@@ -76,7 +75,7 @@ export default {
             initialized: false,
             tableOptions: {
                 columns: this.getTableColumns(),
-                url: "/api/v1/erm/eholdings/local/packages",
+                url: this.getResourceTableUrl(),
                 options: {
                     embed: "resources+count,vendor.name",
                     searchCols: [
@@ -122,46 +121,6 @@ export default {
                 error => {}
             )
         },
-        doShow: function ({ package_id }, dt, event) {
-            event.preventDefault()
-            this.$router.push({
-                name: "EHoldingsLocalPackagesShow",
-                params: { package_id },
-            })
-        },
-        doEdit: function ({ package_id }, dt, event) {
-            this.$router.push({
-                name: "EHoldingsLocalPackagesFormAddEdit",
-                params: { package_id },
-            })
-        },
-        doDelete: function (erm_package, dt, event) {
-            this.setConfirmationDialog(
-                {
-                    title: this.$__(
-                        "Are you sure you want to remove this package?"
-                    ),
-                    message: erm_package.name,
-                    accept_label: this.$__("Yes, delete"),
-                    cancel_label: this.$__("No, do not delete"),
-                },
-                () => {
-                    const client = APIClient.erm
-                    client.localPackages.delete(erm_package.package_id).then(
-                        success => {
-                            this.setMessage(
-                                this.$__("Local package %s deleted").format(
-                                    erm_package.name
-                                ),
-                                true
-                            )
-                            dt.draw()
-                        },
-                        error => {}
-                    )
-                }
-            )
-        },
         getTableColumns: function () {
             let get_lib_from_av = this.get_lib_from_av
             let escape_str = this.escape_str
@@ -173,9 +132,7 @@ export default {
                     orderable: true,
                     render: function (data, type, row, meta) {
                         return (
-                            '<a href="/cgi-bin/koha/erm/eholdings/local/packages/' +
-                            row.package_id +
-                            '" class="show">' +
+                            '<a role="button" class="show">' +
                             escape_str(`${row.name} (#${row.package_id})`) +
                             "</a>"
                         )
