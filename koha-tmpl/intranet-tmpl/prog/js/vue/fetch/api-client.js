32eba273dd9a013de1c5dd6d645d4baa9746af7d
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-10-18 11:53:24 +0000

    Bug 37893: VueJS: SIP2 Accounts

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
@@ -3,6 +3,7 @@ import PatronAPIClient from "./patron-api-client";
 import AcquisitionAPIClient from "./acquisition-api-client";
 import AdditionalFieldsAPIClient from "./additional-fields-api-client";
 import AVAPIClient from "./authorised-values-api-client";
+import CashAPIClient from "./cash-api-client";
 import ItemAPIClient from "./item-api-client";
 import RecordSourcesAPIClient from "./record-sources-api-client";
 import SysprefAPIClient from "./system-preferences-api-client";
@@ -15,6 +16,7 @@ export const APIClient = {
     acquisition: new AcquisitionAPIClient(),
     additional_fields: new AdditionalFieldsAPIClient(),
     authorised_values: new AVAPIClient(),
+    cash: new CashAPIClient(),
     item: new ItemAPIClient(),
     sysprefs: new SysprefAPIClient(),
     sip2: new SIP2APIClient(),
