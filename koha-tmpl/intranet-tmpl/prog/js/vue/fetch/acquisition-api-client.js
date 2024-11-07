a2899a2dde0dee32f11a4d5b49b8c54f3a7e1a2a
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-11-07 13:24:00 +0000

    Bug 38010: Rebase for bug 38390

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/acquisition-api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/acquisition-api-client.js
@@ -14,7 +14,7 @@ export class AcquisitionAPIClient extends HttpClient {
                     endpoint: "vendors/" + id,
                     headers: {
                         "x-koha-embed":
-                            "aliases,subscriptions,interfaces,contacts,contracts",
+                            "aliases,subscriptions+count,interfaces,contacts,contracts",
                     },
                 }),
             getAll: (query, params) =>
