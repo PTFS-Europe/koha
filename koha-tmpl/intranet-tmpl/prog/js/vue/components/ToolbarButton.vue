b2a99b08590afca1f0290b70338b4f5aed9ddcdc
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-11-20 10:12:19 +0000

    Bug 38010: Add callbacks to Toolbarbutton

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ToolbarButton.vue
@@ -17,6 +17,14 @@
         :class="class"
         ><font-awesome-icon icon="pencil" /> {{ $__("Edit") }}</a
     >
+    <a
+        v-else-if="callback"
+        @click="typeof callback === 'string' ? redirect() : callback"
+        :class="class"
+        style="cursor: pointer"
+    >
+        <font-awesome-icon v-if="icon" :icon="icon" /> {{ title }}
+    </a>
     <router-link v-else-if="action === undefined && to" :to="to" :class="class"
         ><font-awesome-icon v-if="icon" :icon="icon" /> {{ title }}</router-link
     >
@@ -42,6 +50,36 @@ export default {
         },
         title: {
             type: String,
+        },
+        callback: {
+            type: [String, Function],
+            required: false,
+        },
+    },
+    methods: {
+        redirect() {
+            if (typeof this.to === "string")
+                window.location.href = this.formatUrl(this.to)
+            if (typeof this.to === "object") {
+                let url = this.to.path
+                if (this.to.hasOwnProperty("query")) {
+                    url +=
+                        "?" +
+                        Object.keys(this.to.query)
+                            .map(
+                                queryParam =>
+                                    `${queryParam}=${this.to.query[queryParam]}`
+                            )
+                            .join("&")
+                }
+                window.open(this.formatUrl(url, this.to.internal), "_blank")
+            }
+        },
+        formatUrl(url) {
+            if (url.includes("http://") || url.includes("https://")) return url
+            if (url.includes("cgi-bin/koha"))
+                return `//${window.location.host}/${url}`
+            return `//${url}`
         },
     },
     emits: ["go-to-add-resource", "go-to-edit-resource", "delete-resource"],
