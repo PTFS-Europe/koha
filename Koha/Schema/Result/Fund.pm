2d575b1f22785c787df8857d58fb9374540c1549
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-11-21 11:41:43 +0000

    DBIC files

---
+++ Koha/Schema/Result/Fund.pm
@@ -0,0 +1,338 @@
+use utf8;
+package Koha::Schema::Result::Fund;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::Fund
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<funds>
+
+=cut
+
+__PACKAGE__->table("funds");
+
+=head1 ACCESSORS
+
+=head2 fund_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 ledger_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+ledger the fund applies to
+
+=head2 fiscal_period_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+fiscal period the fund applies to
+
+=head2 name
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+name for the fund
+
+=head2 description
+
+  data_type: 'longtext'
+  default_value: ''''
+  is_nullable: 1
+
+description for the fund
+
+=head2 fund_type
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+type for the fund
+
+=head2 fund_group_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+group for the fund
+
+=head2 code
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+code for the fund
+
+=head2 external_id
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+external id for the fund for use with external accounting systems
+
+=head2 currency
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 10
+
+currency of the fund
+
+=head2 status
+
+  data_type: 'tinyint'
+  default_value: 1
+  is_nullable: 1
+
+is the fund currently active
+
+=head2 owner_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+owner of the fund
+
+=head2 fund_value
+
+  data_type: 'decimal'
+  default_value: 0.000000
+  is_nullable: 1
+  size: [28,6]
+
+value of the fund
+
+=head2 last_updated
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 1
+
+time of the last update to the fund
+
+=head2 lib_group_visibility
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+library groups the fund is visible to
+
+=cut
+
+__PACKAGE__->add_columns(
+  "fund_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "ledger_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "fiscal_period_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "name",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+  "description",
+  { data_type => "longtext", default_value => "''", is_nullable => 1 },
+  "fund_type",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+  "fund_group_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "code",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+  "external_id",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+  "currency",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 10 },
+  "status",
+  { data_type => "tinyint", default_value => 1, is_nullable => 1 },
+  "owner_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "fund_value",
+  {
+    data_type => "decimal",
+    default_value => "0.000000",
+    is_nullable => 1,
+    size => [28, 6],
+  },
+  "last_updated",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 1,
+  },
+  "lib_group_visibility",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</fund_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("fund_id");
+
+=head1 RELATIONS
+
+=head2 fiscal_period
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::FiscalPeriod>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "fiscal_period",
+  "Koha::Schema::Result::FiscalPeriod",
+  { fiscal_period_id => "fiscal_period_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 fund_allocations
+
+Type: has_many
+
+Related object: L<Koha::Schema::Result::FundAllocation>
+
+=cut
+
+__PACKAGE__->has_many(
+  "fund_allocations",
+  "Koha::Schema::Result::FundAllocation",
+  { "foreign.fund_id" => "self.fund_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 fund_group
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::FundGroup>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "fund_group",
+  "Koha::Schema::Result::FundGroup",
+  { fund_group_id => "fund_group_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "SET NULL",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 ledger
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Ledger>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "ledger",
+  "Koha::Schema::Result::Ledger",
+  { ledger_id => "ledger_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 owner
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Borrower>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "owner",
+  "Koha::Schema::Result::Borrower",
+  { borrowernumber => "owner_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "RESTRICT",
+    on_update     => "RESTRICT",
+  },
+);
+
+=head2 sub_funds
+
+Type: has_many
+
+Related object: L<Koha::Schema::Result::SubFund>
+
+=cut
+
+__PACKAGE__->has_many(
+  "sub_funds",
+  "Koha::Schema::Result::SubFund",
+  { "foreign.fund_id" => "self.fund_id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2024-11-20 14:37:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pl+BLQTapHPx0MNtq/xmSQ
+
+__PACKAGE__->add_columns(
+    '+status' => { is_boolean => 1 },
+);
+
+sub koha_object_class {
+    'Koha::Acquisition::FundManagement::Fund';
+}
+
+sub koha_objects_class {
+    'Koha::Acquisition::FundManagement::Funds';
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ Koha/Schema/Result/FundAllocation.pm
@@ -0,0 +1,305 @@
+use utf8;
+package Koha::Schema::Result::FundAllocation;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::FundAllocation
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<fund_allocation>
+
+=cut
+
+__PACKAGE__->table("fund_allocation");
+
+=head1 ACCESSORS
+
+=head2 fund_allocation_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 fund_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+fund the fund allocation applies to
+
+=head2 sub_fund_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+sub fund the fund allocation applies to
+
+=head2 ledger_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+ledger the fund allocation applies to
+
+=head2 fiscal_period_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+fiscal period the fund allocation applies to
+
+=head2 allocation_amount
+
+  data_type: 'decimal'
+  default_value: 0.000000
+  is_nullable: 1
+  size: [28,6]
+
+amount for the allocation
+
+=head2 reference
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+allocation reference
+
+=head2 note
+
+  data_type: 'longtext'
+  default_value: ''''
+  is_nullable: 1
+
+any notes associated to the allocation
+
+=head2 currency
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 10
+
+currency of the fund allocation
+
+=head2 owner_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+owner of the fund allocation
+
+=head2 is_transfer
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 1
+
+is the fund allocation a transfer to/from another fund
+
+=head2 last_updated
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 1
+
+time of the last update to the fund allocation
+
+=head2 lib_group_visibility
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 1
+  size: 255
+
+library groups the fund allocation is visible to
+
+=cut
+
+__PACKAGE__->add_columns(
+  "fund_allocation_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "fund_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "sub_fund_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "ledger_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "fiscal_period_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "allocation_amount",
+  {
+    data_type => "decimal",
+    default_value => "0.000000",
+    is_nullable => 1,
+    size => [28, 6],
+  },
+  "reference",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+  "note",
+  { data_type => "longtext", default_value => "''", is_nullable => 1 },
+  "currency",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 10 },
+  "owner_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "is_transfer",
+  { data_type => "tinyint", default_value => 0, is_nullable => 1 },
+  "last_updated",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 1,
+  },
+  "lib_group_visibility",
+  { data_type => "varchar", default_value => "", is_nullable => 1, size => 255 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</fund_allocation_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("fund_allocation_id");
+
+=head1 RELATIONS
+
+=head2 fiscal_period
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::FiscalPeriod>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "fiscal_period",
+  "Koha::Schema::Result::FiscalPeriod",
+  { fiscal_period_id => "fiscal_period_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 fund
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Fund>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "fund",
+  "Koha::Schema::Result::Fund",
+  { fund_id => "fund_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 ledger
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Ledger>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "ledger",
+  "Koha::Schema::Result::Ledger",
+  { ledger_id => "ledger_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 owner
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Borrower>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "owner",
+  "Koha::Schema::Result::Borrower",
+  { borrowernumber => "owner_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "RESTRICT",
+    on_update     => "RESTRICT",
+  },
+);
+
+=head2 sub_fund
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::SubFund>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "sub_fund",
+  "Koha::Schema::Result::SubFund",
+  { sub_fund_id => "sub_fund_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2024-11-20 14:37:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:KC3yQHE32XPTWHR6PZZeVg
+
+__PACKAGE__->add_columns(
+    '+is_transfer' => { is_boolean => 1 },
+);
+
+sub koha_object_class {
+    'Koha::Acquisition::FundManagement::FundAllocation';
+}
+
+sub koha_objects_class {
+    'Koha::Acquisition::FundManagement::FundAllocations';
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
