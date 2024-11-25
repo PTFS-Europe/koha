d611430058a79cab612b1952a088aeeac4d97c7d
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-11-25 14:22:54 +0000

    Show component

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ShowElement.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ShowElement.vue
@@ -1,27 +1,28 @@
 <template>
-    <div v-if="attr.type == 'text' || attr.type == 'textarea'">
+    <div
+        v-if="
+            attr.type == 'text' ||
+            attr.type == 'textarea' ||
+            attr.showElement?.type == 'text'
+        "
+    >
         <label>{{ attr.label }}:</label>
-        <span>
-            {{ resource[attr.name] }}
-        </span>
+        <LinkWrapper :linkData="attr.showElement?.link" :resource="resource">
+            <span>
+                {{
+                    attr.showElement?.format
+                        ? attr.showElement?.format(
+                              attr.showElement.value,
+                              resource
+                          )
+                        : resource[attr.name]
+                }}
+            </span>
+        </LinkWrapper>
     </div>
     <div v-else-if="attr.type == 'av'">
         <label>{{ attr.label }}:</label>
         <span>{{ get_lib_from_av(attr.av_cat, resource[attr.name]) }}</span>
-    </div>
-    <div
-        v-else-if="
-            attr.type == 'component' && attr.component == 'FormSelectVendors'
-        "
-    >
-        <label>{{ attr.label }}:</label>
-        <span v-if="resource[attr.name]">
-            <a
-                :href="`/cgi-bin/koha/acqui/booksellers.pl?booksellerid=${resource['vendor_id']}`"
-            >
-                {{ resource["vendor"]["name"] }}
-            </a>
-        </span>
     </div>
     <div
         v-else-if="
@@ -92,11 +93,20 @@
             </table>
         </template>
     </div>
+    <div v-else-if="attr.showElement?.type === 'component'">
+        <template v-if="attr.showElement.hidden(resource)">
+            <component
+                :is="requiredComponent"
+                v-bind="requiredProps()"
+            ></component>
+        </template>
+    </div>
 </template>
 
 <script>
 import { inject } from "vue"
 import LinkWrapper from "./LinkWrapper.vue"
+import { defineAsyncComponent } from "vue"
 
 export default {
     components: { LinkWrapper },
@@ -111,6 +121,57 @@ export default {
     props: {
         resource: null,
         attr: null,
+    },
+    computed: {
+        requiredComponent() {
+            return defineAsyncComponent(() =>
+                import(`${this.attr.showElement.componentPath}`)
+            )
+        },
+        selectOptions() {
+            if (this.attr.options) {
+                return this.attr.options
+            }
+            return this.options
+        },
+    },
+    methods: {
+        requiredProps() {
+            if (!this.attr.showElement.props) {
+                return {}
+            }
+            const props = Object.keys(this.attr.showElement.props).reduce(
+                (acc, key) => {
+                    // This might be better in a switch statement
+                    const prop = this.attr.showElement.props[key]
+                    if (prop.type === "resource") {
+                        acc[key] = this.resource
+                    }
+                    if (prop.type === "resourceProperty") {
+                        acc[key] = this.resource[prop.resourceProperty]
+                    }
+                    if (prop.type === "av") {
+                        acc[key] = prop.av
+                    }
+                    if (prop.type === "string") {
+                        if (prop.indexRequired && this.index > -1) {
+                            acc[key] = `${prop.value}${this.index}`
+                        } else {
+                            acc[key] = prop.value
+                        }
+                    }
+                    if (prop.type === "boolean") {
+                        acc[key] = prop.value
+                    }
+                    return acc
+                },
+                {}
+            )
+            if (this.attr.showElement.subFields?.length) {
+                props.subFields = this.attr.showElement.subFields
+            }
+            return props
+        },
     },
     name: "ShowElement",
 }
