85e7ca5a6eb4ea339968ae150c966ad73936b556
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-12-11 10:49:42 +0000

    Add Vue app internals

---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/routes/acquisitions/fundManagement.js
@@ -0,0 +1,242 @@
+import { markRaw } from "vue";
+
+import Homepage from "../../components/Acquisitions/FundManagement/Homepage.vue";
+import FundManagementHome from "../../components/Acquisitions/FundManagement/FundManagementHome.vue";
+import FiscalPeriodList from "../../components/Acquisitions/FundManagement/FiscalPeriodList.vue";
+import FiscalPeriodShow from "../../components/Acquisitions/FundManagement/FiscalPeriodShow.vue";
+import FiscalPeriodFormAdd from "../../components/Acquisitions/FundManagement/FiscalPeriodFormAdd.vue";
+import LedgerList from "../../components/Acquisitions/FundManagement/LedgerList.vue";
+import LedgerShow from "../../components/Acquisitions/FundManagement/LedgerShow.vue";
+import LedgerFormAdd from "../../components/Acquisitions/FundManagement/LedgerFormAdd.vue";
+import FundList from "../../components/Acquisitions/FundManagement/FundList.vue";
+import FundShow from "../../components/Acquisitions/FundManagement/FundShow.vue";
+import FundFormAdd from "../../components/Acquisitions/FundManagement/FundFormAdd.vue";
+import SubFundFormAdd from "../../components/Acquisitions/FundManagement/SubFundFormAdd.vue";
+import FundGroupList from "../../components/Acquisitions/FundManagement/FundGroupList.vue";
+import FundGroupShow from "../../components/Acquisitions/FundManagement/FundGroupShow.vue";
+import FundGroupFormAdd from "../../components/Acquisitions/FundManagement/FundGroupFormAdd.vue";
+import FundAllocationShow from "../../components/Acquisitions/FundManagement/FundAllocationShow.vue";
+import FundAllocationFormAdd from "../../components/Acquisitions/FundManagement/FundAllocationFormAdd.vue";
+import TransferFunds from "../../components/Acquisitions/FundManagement/TransferFunds.vue";
+
+import { $__ } from "../../i18n";
+
+export const routes = [
+    {
+        path: "/cgi-bin/koha/acqui/acqui-home.pl",
+        is_default: true,
+        is_base: true,
+        title: $__("Acquisitions"),
+        children: [
+            {
+                path: "",
+                name: "Home",
+                component: markRaw(Homepage),
+                is_navigation_item: false,
+            },
+            {
+                path: "/cgi-bin/koha/fund_management",
+                moduleName: "funds",
+                title: "Funds and ledgers",
+                icon: "fa fa-money-check-dollar",
+                children: [
+                    {
+                        path: "",
+                        component: markRaw(FundManagementHome),
+                        name: "FundManagementHome",
+                        is_navigation_item: false,
+                    },
+                    {
+                        path: "fiscal_period",
+                        title: "Fiscal periods",
+                        is_navigation_item: false,
+                        children: [
+                            {
+                                path: "",
+                                component: markRaw(FiscalPeriodList),
+                                name: "FiscalPeriodList",
+                                title: "List fiscal periods",
+                                permission: "manageFiscalPeriods",
+                            },
+                            {
+                                path: ":fiscal_period_id",
+                                component: markRaw(FiscalPeriodShow),
+                                name: "FiscalPeriodShow",
+                                title: "Show fiscal period",
+                                permission: "manageFiscalPeriods",
+                            },
+                            {
+                                path: "add",
+                                component: markRaw(FiscalPeriodFormAdd),
+                                name: "FiscalPeriodFormAdd",
+                                title: "Add fiscal period",
+                                permission: "createFiscalPeriods",
+                            },
+                            {
+                                path: "edit/:fiscal_period_id",
+                                component: markRaw(FiscalPeriodFormAdd),
+                                name: "FiscalPeriodFormEdit",
+                                title: "Edit fiscal period",
+                                permission: "editFiscalPeriod",
+                            },
+                        ],
+                    },
+                    {
+                        path: "ledger",
+                        title: "Ledgers",
+                        is_navigation_item: false,
+                        children: [
+                            {
+                                path: "",
+                                component: markRaw(LedgerList),
+                                name: "LedgerList",
+                                title: "List ledgers",
+                                permission: "manageLedgers",
+                            },
+                            {
+                                path: ":ledger_id",
+                                component: markRaw(LedgerShow),
+                                name: "LedgerShow",
+                                title: "Show ledger",
+                                permission: "manageLedgers",
+                            },
+                            {
+                                path: "add",
+                                component: markRaw(LedgerFormAdd),
+                                name: "LedgerFormAdd",
+                                title: "Add ledger",
+                                permission: "createLedger",
+                            },
+                            {
+                                path: "edit/:ledger_id",
+                                component: markRaw(LedgerFormAdd),
+                                name: "LedgerFormEdit",
+                                title: "Edit ledger",
+                                permission: "editLedger",
+                            },
+                        ],
+                    },
+                    {
+                        path: "fund",
+                        title: "Funds",
+                        is_navigation_item: false,
+                        children: [
+                            {
+                                path: "",
+                                component: markRaw(FundList),
+                                name: "FundList",
+                                title: "List funds",
+                                permission: "manageFunds",
+                            },
+                            {
+                                path: ":fund_id",
+                                component: markRaw(FundShow),
+                                name: "FundShow",
+                                title: "Show fund",
+                                permission: "manageFunds",
+                            },
+                            {
+                                path: "add",
+                                component: markRaw(FundFormAdd),
+                                name: "FundFormAdd",
+                                title: "Add fund",
+                                permission: "createFund",
+                            },
+                            {
+                                path: "edit/:fund_id",
+                                component: markRaw(FundFormAdd),
+                                name: "FundFormEdit",
+                                title: "Edit fund",
+                                permission: "editFund",
+                            },
+                            {
+                                path: ":fund_id/allocation",
+                                component: markRaw(FundAllocationShow),
+                                name: "FundAllocationShow",
+                                title: "Show fund allocation",
+                                permission: "manageFundAllocations",
+                            },
+                            {
+                                path: ":fund_id/allocation/edit/:fund_allocation_id",
+                                component: markRaw(FundAllocationFormAdd),
+                                name: "FundAllocationFormEdit",
+                                title: "Edit fund allocation",
+                                permission: "editFundAllocation",
+                            },
+                            {
+                                path: ":fund_id/:sub_fund_id?/allocate",
+                                component: markRaw(FundAllocationFormAdd),
+                                name: "FundAllocationFormAdd",
+                                title: "Allocate funds",
+                                permission: "createFundAllocation",
+                            },
+                            {
+                                path: "transfer",
+                                component: markRaw(TransferFunds),
+                                name: "TransferFunds",
+                                title: "Transfer funds",
+                                permission: "createFundAllocation",
+                            },
+                            {
+                                path: ":fund_id/sub_fund/add",
+                                component: markRaw(SubFundFormAdd),
+                                name: "SubFundFormAdd",
+                                title: "Add sub fund",
+                                permission: "createFund",
+                            },
+                            {
+                                path: ":fund_id/sub_fund/edit/:sub_fund_id",
+                                component: markRaw(SubFundFormAdd),
+                                name: "SubFundFormAddEdit",
+                                title: "Edit sub fund",
+                                permission: "editFund",
+                            },
+                            {
+                                path: "sub_fund/:sub_fund_id",
+                                component: markRaw(FundShow),
+                                name: "SubFundShow",
+                                title: "Show sub fund",
+                                permission: "manageFunds",
+                            },
+                        ],
+                    },
+                    {
+                        path: "fund_group",
+                        title: "Fund groups",
+                        is_navigation_item: false,
+                        children: [
+                            {
+                                path: "",
+                                component: markRaw(FundGroupList),
+                                name: "FundGroupList",
+                                title: "List fund groups",
+                                permission: "manageFundGroups",
+                            },
+                            {
+                                path: ":fund_group_id",
+                                component: markRaw(FundGroupShow),
+                                name: "FundGroupShow",
+                                title: "Show fund group",
+                                permission: "manageFundGroups",
+                            },
+                            {
+                                path: "add",
+                                component: markRaw(FundGroupFormAdd),
+                                name: "FundGroupFormAdd",
+                                title: "Add fund group",
+                                permission: "createFundGroup",
+                            },
+                            {
+                                path: "edit/:fund_group_id",
+                                component: markRaw(FundGroupFormAdd),
+                                name: "FundGroupFormEdit",
+                                title: "Edit fund group",
+                                permission: "editFundGroup",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/stores/acquisitions.js
@@ -0,0 +1,268 @@
+import { defineStore } from "pinia";
+import { permissionsMatrix } from "../data/permissionsMatrix";
+
+export const useAcquisitionsStore = defineStore("acquisitions", {
+    state: () => ({
+        user: {
+            loggedInUser: null,
+            userflags: null,
+        },
+        libraryGroups: null,
+        settings: null,
+        permittedUsers: null,
+        visibleGroups: null,
+        owners: null,
+        navigationBlocked: false,
+        currentPermission: null,
+        moduleList: {
+            funds: { name: "Funds and ledgers", code: "funds" },
+        },
+        permissionsMatrix: permissionsMatrix,
+        currencies: null,
+    }),
+    actions: {
+        determineBranch(code) {
+            if (code) {
+                return code;
+            }
+            const {
+                loggedInUser: { loggedInBranch, branchcode },
+            } = this.user;
+            return loggedInBranch ? loggedInBranch : branchcode;
+        },
+        _mapSubGroups(group, filteredGroups, branch, groupsToCheck) {
+            let matched = false;
+            if (group.libraries.find(lib => lib.branchcode === branch)) {
+                if (
+                    groupsToCheck &&
+                    groupsToCheck.length &&
+                    groupsToCheck.includes(group.id)
+                ) {
+                    filteredGroups[group.id] = group;
+                    matched = true;
+                }
+                if (!groupsToCheck || groupsToCheck.length === 0) {
+                    filteredGroups[group.id] = group;
+                    matched = true;
+                }
+            }
+            if (group.sub_groups && group.sub_groups.length) {
+                group.sub_groups.forEach(grp => {
+                    const result = this._mapSubGroups(
+                        grp,
+                        filteredGroups,
+                        branch,
+                        groupsToCheck
+                    );
+                    matched = matched ? matched : result;
+                });
+            }
+            return matched;
+        },
+        filterLibGroupsByUsersBranchcode(branchcode, groupsToCheck) {
+            const branch = this.determineBranch(branchcode);
+            const filteredGroups = {};
+            this.libraryGroups.forEach(group => {
+                const matched = this._mapSubGroups(
+                    group,
+                    filteredGroups,
+                    branch,
+                    groupsToCheck
+                );
+                // If a sub group has been matched but the parent level group did not, then we should add the parent level group as well
+                // This happens when a parent group doesn't have any branchcodes assigned to it, only sub groups
+                if (
+                    matched &&
+                    !Object.keys(filteredGroups).find(id => id === group.id)
+                ) {
+                    filteredGroups[group.id] = group;
+                }
+            });
+            return Object.keys(filteredGroups)
+                .map(key => {
+                    return filteredGroups[key];
+                })
+                .sort((a, b) => a.id - b.id);
+        },
+        _findBranchCodesInGroup(groups) {
+            const codes = [];
+            groups.forEach(group => {
+                group.libraries.forEach(lib => {
+                    if (!codes.find(code => code === lib.branchcode)) {
+                        codes.push(lib.branchcode);
+                    }
+                });
+            });
+            return codes;
+        },
+        filterUsersByPermissions(
+            operation,
+            branchcodes = null,
+            returnAll = false
+        ) {
+            const filteredUsers = [];
+            this.permittedUsers.forEach(user => {
+                user.displayName = user.firstname + " " + user.surname;
+                if (returnAll) {
+                    filteredUsers.push(user);
+                } else {
+                    const userPermitted = this.isUserPermitted(
+                        operation,
+                        user.permissions
+                    );
+                    if (userPermitted) {
+                        filteredUsers.push(user);
+                    }
+                }
+            });
+            if (branchcodes) {
+                return filteredUsers.filter(user =>
+                    branchcodes.includes(user.branchcode)
+                );
+            } else {
+                return filteredUsers;
+            }
+        },
+        isUserPermitted(operation, flags) {
+            const userflags = flags ? flags : this.user.userflags;
+            if (!operation) return true;
+            if (this.permissionsMatrix[operation].length === 0) return true;
+
+            const { acquisition, parameters, superlibrarian } = userflags;
+            if (operation === "manageSettings") {
+                let checkResult = false;
+                if (
+                    superlibrarian ||
+                    parameters === 1 ||
+                    parameters.manage_sysprefs
+                ) {
+                    checkResult = true;
+                } else {
+                    checkResult = false;
+                }
+                return checkResult;
+            }
+
+            if (acquisition === 1 || superlibrarian) {
+                return true;
+            } else {
+                const checks = this.permissionsMatrix[operation].map(
+                    permission => {
+                        if (acquisition[permission]) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                );
+                const failedChecks = checks.filter(check => !check).length;
+                return failedChecks > 0 ? false : true;
+            }
+        },
+        filterGroupsBasedOnOwner(e, data, groups) {
+            const libGroups = this.filterLibGroupsByUsersBranchcode(
+                null,
+                groups
+            );
+            const permittedUsers = this.filterUsersByPermissions(
+                this.currentPermission
+            );
+            if (!e) {
+                this.visibleGroups = libGroups;
+                this.owners = permittedUsers;
+                data.lib_group_visibility = null;
+            } else {
+                const { branchcode } = permittedUsers.find(
+                    user => user.borrowernumber === e
+                );
+                this.visibleGroups = this.filterLibGroupsByUsersBranchcode(
+                    branchcode,
+                    groups
+                );
+            }
+        },
+        filterOwnersBasedOnGroup(e, data, groups) {
+            const libGroups = this.filterLibGroupsByUsersBranchcode(
+                null,
+                groups
+            );
+            const permittedUsers = this.filterUsersByPermissions(
+                this.currentPermission
+            );
+            if (!e.length) {
+                this.visibleGroups = libGroups;
+                this.owners = permittedUsers;
+                data.owner_id = null;
+            } else {
+                const filteredGroups = libGroups.filter(group =>
+                    e.includes(group.id)
+                );
+                const branchcodes =
+                    this._findBranchCodesInGroup(filteredGroups);
+                this.owners = this.filterUsersByPermissions(
+                    this.currentPermission,
+                    branchcodes
+                );
+            }
+        },
+        setOwnersBasedOnPermission(permission) {
+            if (this.permittedUsers) {
+                this.owners = this.filterUsersByPermissions(permission);
+            }
+        },
+        resetOwnersAndVisibleGroups(groups) {
+            this.owners = this.filterUsersByPermissions(this.currentPermission);
+            this.visibleGroups = this.filterLibGroupsByUsersBranchcode(
+                null,
+                groups
+            );
+        },
+        getSetting(input) {
+            if (typeof input === "string") {
+                return this.settings[input];
+            } else {
+                return input.map(setting => {
+                    return this.settings[setting];
+                });
+            }
+        },
+        convertSettingsToObject(settings) {
+            const settingsObject = {};
+            settings.forEach(setting => {
+                settingsObject[setting.variable] = setting;
+            });
+            return settingsObject;
+        },
+        formatLibraryGroupIds(ids) {
+            const groups = ids.includes("|") ? ids.split("|") : [ids];
+            const groupIds = groups.map(group => {
+                return parseInt(group);
+            });
+            return groupIds;
+        },
+        formatValueWithCurrency(currency, value) {
+            const { symbol } = this.currencies.find(
+                curr => curr.currency === currency
+            );
+            if (!value) {
+                return `${symbol}0`;
+            }
+            if (value < 0) {
+                return `-${symbol}${-value}`;
+            }
+            return `${symbol}${value}`;
+        },
+    },
+    getters: {
+        modulesEnabled() {
+            const modulesEnabled = this.settings.modulesEnabled;
+            return modulesEnabled.value ? modulesEnabled.value : "";
+        },
+        getVisibleGroups() {
+            return this.visibleGroups;
+        },
+        getOwners() {
+            return this.owners;
+        },
+    },
+});
