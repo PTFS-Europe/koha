7bed41c0c81ac18b81a04c295206d22872392c8c
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-12-13 17:06:56 +0000

    UAL: Add an edi_fix cron script
    Add a script to run after the edi_cron has imported quotes. It should find any orderlines
    added during the day, look for the EDI populated vendornote and map the content into fields
    appropriately as per support requests.
    
    Currently we map as follows:
    * $Dollar quoted strings$ go into sort1
    * ^Hat quoted strings^ are appended to order_internalnote
    * #Hash quoted strings# go into sort2

---
+++ misc/cronjobs/edi_fix.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+# Copyright 2024 PTFS Europe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use Modern::Perl;
+use DateTime;
+
+use Koha::Script -cron;
+use C4::Context;
+use Koha::Acquisition::Orders;
+use Koha::Database;
+use Koha::Logger;
+
+die "Syspref 'EDIFACT' is disabled" unless C4::Context->preference('EDIFACT');
+
+my $schema = Koha::Database->new()->schema();
+
+process_quotes();
+
+sub process_quotes {
+    my $today  = DateTime->now->ymd;
+    my $orders = Koha::Acquisition::Orders->search(
+        {
+            'me.entrydate' => $today,
+        },
+    );
+
+    while ( my $order = $orders->next ) {
+        my $vendor_note = $order->order_vendornote;
+
+        # Process fields quoted by '$'
+        # Subject fund -> sort1
+        if ( my @matches = $vendor_note =~ /\$(.*?)\$/g ) {
+            if ( @matches > 1 ) {
+                warn "Multiple fields quoted by \$ found: @matches\n";
+            } else {
+                my $sort1 = $matches[0];
+                $order->sort1($sort1);
+            }
+        }
+
+        # Process fields quoted by '^'
+        # Internal reference -> order_internalnote
+        if ( my @matches = $vendor_note =~ /\^(.*?)\^/g ) {
+            if ( @matches > 1 ) {
+                warn "Multiple fields quoted by ^ found: @matches\n";
+            } else {
+                my $order_internalnote =
+                    $order->order_internalnote ? $order->order_internalnote . ' ' . $matches[0] : $matches[0];
+                $order->order_internalnote($order_internalnote);
+            }
+        }
+
+        # Process fields quoted by '#'
+        # Purhchase reason -> sort2
+        if ( my @matches = $vendor_note =~ /\#(.*?)\#/g ) {
+            if ( @matches > 1 ) {
+                warn "Multiple fields quoted by # found: @matches\n";
+            } else {
+                my $sort2 = $matches[0];
+                $order->sort2($sort2);
+            }
+        }
+
+        $order->store();
+    }
+}
