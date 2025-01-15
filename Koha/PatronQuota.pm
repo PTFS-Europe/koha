e76fe72ec55d939dbbb4f4a0777ac91516d62adc
Jacob O'Mara <jacobomara901@gmail.com>
2025-01-15 14:58:35 +0000

    create basic accessor methods

--- Koha/PatronQuota.pm
+++ Koha/PatronQuota.pm
@@ -1,6 +1,7 @@
 package Koha::PatronQuota;
 
 use Modern::Perl;
+use Koha::DateUtils qw( dt_from_string );
 use base qw(Koha::Object);
 
 =head1 NAME
@@ -19,5 +20,78 @@ sub _type {
     return 'PatronQuota';
 }
 
+=head3 add_to_quota
+
+Adds specified amount to the quota_used value
+
+=cut
+
+sub add_to_quota {
+    my ($self, $amount) = @_;
+    
+    my $new_used = $self->quota_used + $amount;
+    $self->quota_used($new_used);
+    return $self->store;
+}
+
+=head3 has_available_quota
+
+Returns boolean indicating if there is quota available
+
+=cut
+
+sub has_available_quota {
+    my ($self) = @_;
+    return $self->available_quota >= 0;
+}
+
+=head3 available_quota
+
+Returns the amount still available in the quota
+
+=cut
+
+sub available_quota {
+    my ($self) = @_;
+    return $self->quota_total - $self->quota_used;
+}
+
+=head2 Instance Methods
+
+=head3 get_patron
+
+Returns the patron object associated with this quota
+
+=cut
+
+sub get_patron {
+    my ($self) = @_;
+    return Koha::Patrons->find($self->patron_id);
+}
+
+=head3 is_active
+
+Returns boolean indicating if the quota period includes today's date
+
+=cut
+
+sub is_active {
+    my ($self) = @_;
+    my $today = dt_from_string;
+    
+    my $start = DateTime->new(
+        year => substr($self->period_start, 0, 4),
+        month => substr($self->period_start, 5, 2),
+        day => substr($self->period_start, 8, 2)
+    );
+    
+    my $end = DateTime->new(
+        year => substr($self->period_end, 0, 4),
+        month => substr($self->period_end, 5, 2),
+        day => substr($self->period_end, 8, 2)
+    );
+    
+    return ($start <= $today && $end >= $today);
+}
 
 1;
--- Koha/PatronQuotas.pm
+++ Koha/PatronQuotas.pm
@@ -1,4 +1,5 @@
 package Koha::PatronQuotas;
+
 use Modern::Perl;
 use Koha::PatronQuota;
 use base qw(Koha::Objects);
@@ -20,8 +21,60 @@ sub _type {
 sub object_class {
     return 'Koha::PatronQuota';
 }
+
+=head2 Class Methods
+
+=head3 get_patron_quota
+
+Returns the active quota for a given patron
+
+=cut
+
+sub get_patron_quota {
+    my ($self, $patron_id) = @_;
+    
+    return $self->search({
+        patron_id => $patron_id,
+        period_start => {'<=' => \'CURRENT_DATE'},
+        period_end => {'>=' => \'CURRENT_DATE'},
+    })->single;
 }
+
+=head3 create_quota
+
+Creates a new quota record for a patron
+
+=cut
+
+sub create_quota {
+    my ($self, $params) = @_;
+    
+    return Koha::PatronQuota->new($params)->store;
 }
+
+=head3 search_by_patron
+
+Finds all quotas for a patron
+
+=cut
+
+sub search_by_patron {
+    my ($self, $patron_id) = @_;
+    return $self->search({ patron_id => $patron_id });
+}
+
+=head3 get_active_quotas
+
+Returns multiple active quota records
+
+=cut
+
+sub get_active_quotas {
+    my ($self) = @_;
+    return $self->search({
+        period_start => {'<=' => \'CURRENT_DATE'},
+        period_end => {'>=' => \'CURRENT_DATE'}
+    });
 }
 
 1;
