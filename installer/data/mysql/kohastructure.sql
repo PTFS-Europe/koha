5eacf985d0ddc3ea02db02f84d9fd0d437de870e
Jacob O'Mara <jacobomara901@gmail.com>
2025-01-15 14:53:57 +0000

    Bug 38924: Add patron_quotas table
    Add the new patron_quotas table for storing circulation quotas

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -6788,6 +6788,28 @@ CREATE TABLE `zebraqueue` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `patron_quota`
+--
+
+DROP TABLE IF EXISTS `patron_quota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patron_quota` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier for the quota record',
+  `description` longtext NOT NULL COMMENT 'user friendly description for the quota record',
+  `patron_id` int(11) NOT NULL COMMENT 'foreign key linking to borrowers.borrowernumber',
+  `allocation` int(11) NOT NULL DEFAULT 0 COMMENT 'total quota allocation for the period',
+  `used` int(11) NOT NULL DEFAULT 0 COMMENT 'amount of allocation used for current period',
+  `start_date` date NOT NULL COMMENT 'start date of the allocation period',
+  `end_date` date NOT NULL COMMENT 'end date of the allocation period',
+  PRIMARY KEY (`id`),
+  KEY `patron_quota_ibfk_1` (`patron_id`),
+  CONSTRAINT `patron_quota_ibfk_1` FOREIGN KEY (`patron_id`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
