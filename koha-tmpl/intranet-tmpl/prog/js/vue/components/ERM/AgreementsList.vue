d4bf57554b1ced9a22d7af87eae323975d5ca2b8
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2025-01-17 17:05:46 +0000

    Bug 38930: (DO NOT PUSH) Add an example to ERM

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementsList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementsList.vue
@@ -6,6 +6,9 @@
                 :to="{ name: 'AgreementsFormAdd' }"
                 icon="plus"
                 :title="$__('New agreement')"
+                v-if="
+                    isUserPermitted('CAN_user_suggestions_suggestions_create')
+                "
             />
         </Toolbar>
         <fieldset v-if="agreement_count > 0" class="filters">
@@ -72,6 +75,8 @@ export default {
         const vendorStore = inject("vendorStore");
         const { vendors } = storeToRefs(vendorStore);
 
+        const { isUserPermitted } = inject("permissionsStore");
+
         const { setConfirmationDialog, setMessage } = inject("mainStore");
 
         const { get_lib_from_av, map_av_dt_filter } = inject("ERMStore");
@@ -94,6 +99,7 @@ export default {
             escape_str,
             agreement_table_settings,
             filters,
+            isUserPermitted,
         };
     },
     data: function () {
