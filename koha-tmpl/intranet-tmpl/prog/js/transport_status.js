898a589f7aa919c8980611bb641dfbbf346ed299
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2025-01-22 12:38:59 +0000

    Bug 35761: Re-implement the connection testing
    We now use background jobs triggered by Transport->store to update the
    connection status.
    
    Status is now a json blob with more details, so we update the api
    definition to reflect that.

---
+++ koha-tmpl/intranet-tmpl/prog/js/transport_status.js
@@ -0,0 +1,7 @@
+let operationLabels = {
+    connection: _("Connection"),
+    upload: _("Upload"),
+    download: _("Download"),
+    list: _("List Files"),
+    change_directory: _("Change Directory"),
+};
