8f48268bcd41149f4921af04cd12b5e0f7f0faac
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2025-01-28 16:17:20 +0000

    Bug 38924: Start adding scaffolding for quota management page
    This adds the basic scaffolding for a new patron quota management tab
    under the members area.
    
    Depending on permissions, we will need display only options including
    guarantor and guarantee usage against quotas and also the options to add
    and edit quota.

---
+++ members/circulation_quota.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+# Copyright 2025 PTFS Europe
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+
+use C4::Auth        qw( get_template_and_user );
+use C4::Output      qw( output_and_exit_if_error output_and_exit output_html_with_http_headers );
+use List::MoreUtils qw( any uniq );
+use Koha::DateUtils qw( dt_from_string );
+use Koha::ActionLogs;
+
+use Koha::Patrons;
+use Koha::Patron::Categories;
+
+my $input = CGI->new;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name => "members/circulation_quota.tt",
+        query         => $input,
+        type          => "intranet",
+        flagsrequired => { borrowers => 'edit_borrowers' },
+    }
+);
+
+my $op = $input->param('op') || '';
+my $patron;
+if ( $input->param('borrowernumber') ) {
+    my $borrowernumber = $input->param('borrowernumber');
+    $patron = Koha::Patrons->find($borrowernumber);
+}
+
+my $logged_in_user = Koha::Patrons->find($loggedinuser);
+output_and_exit_if_error(
+    $input, $cookie, $template,
+    { module => 'members', logged_in_user => $logged_in_user, current_patron => $patron }
+);
+
+$template->param(
+    patron => $patron,
+);
+output_html_with_http_headers $input, $cookie, $template->output;
+
