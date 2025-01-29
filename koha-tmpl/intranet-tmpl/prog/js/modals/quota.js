0a0f5aa75436cc99bbdaa20dd064553a7d7be5a2
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2025-01-29 09:33:19 +0000

    Bug 38924: Start adding modal for adding/editing quota

---
+++ koha-tmpl/intranet-tmpl/prog/js/modals/quota.js
@@ -0,0 +1,83 @@
+(() => {
+    document
+        .getElementById("quotaModal")
+        ?.addEventListener("show.bs.modal", handleShowBsModal);
+    document
+        .getElementById("quotaForm")
+        ?.addEventListener("submit", handleSubmit);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const target = e.target;
+        if (!(target instanceof HTMLFormElement)) {
+            return;
+        }
+
+        const formData = new FormData(target);
+        const quotaId = formData.get("quota_id");
+        const patronId = formData.get("patron_id");
+
+        const quotaUrl = quotaId ? `/api/v1/patrons/${patronId}/quotas/${quotaId}` : `/api/v1/patrons/${patronId}/quotas`;
+        let [error, response] = await catchError(
+            fetch( quotaUrl, {
+                method: quotaId ? "PUT" : "POST",
+                body: JSON.stringify({
+                    patron_id: patronId, 
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+        );
+        if (error || !response.ok) {
+            const alertContainer = document.getElementById(
+                "quota_result"
+            );
+            alertContainer.outerHTML = `
+                <div id="quota_result" class="alert alert-danger">
+                    ${__("Failure")}
+                </div>
+            `;
+
+            return;
+        }
+
+        quota_success = true;
+        quotas_table?.api().ajax.reload();
+
+        $("#quotaModal").modal("hide");
+    }
+
+    function handleShowBsModal(e) {
+        const button = e.relatedTarget;
+        if (!button) {
+            return;
+        }
+
+        const quotaModalLabel = document.getElementById("quotaLabel");
+        const quotaModalSubmit = document.getElementById("quotaFormSubmit");
+
+        const quota = button.dataset.quota;
+        const quotaIdInput = document.getElementById("quota_id");
+        if (quota) {
+            quotaIdInput.value = quota;
+            quotaModalLabel.textContent = _("Edit quote");
+            quotaModalSubmit.innerHTML = "<i class=\"fa fa-check\"></i> " + _("Update");
+        } else {
+            quotaIdInput.value = null;
+            quotaModalLabel.textContent = _("Add quota");
+            quotaModalSubmit.innerHTML = "<i class=\"fa fa-fw fa-plus\"></i> " + _("Add");
+        }
+
+        const patron = button.dataset.borrowernumber;
+        const quotaPatronInput = document.getElementById("quota_patron_id");
+        quotaPatronInput.value = patron;
+
+        return;
+    }
+
+    function catchError(promise) {
+        return promise.then(data => [undefined, data]).catch(error => [error]);
+    }
+})();
