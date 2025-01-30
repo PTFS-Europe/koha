95323cc4e9f61f3b59b6041cbddb50ed1a4ab0eb
Jacob O'Mara <jacobomara901@gmail.com>
2025-01-30 16:16:17 +0000

    Bug 38924: DBIC patron_quota_usage

---
+++ Koha/Schema/Result/PatronQuotaUsage.pm
@@ -0,0 +1,141 @@
+use utf8;
+package Koha::Schema::Result::PatronQuotaUsage;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::PatronQuotaUsage
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<patron_quota_usage>
+
+=cut
+
+__PACKAGE__->table("patron_quota_usage");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+unique identifier for quota usage record
+
+=head2 patron_quota_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+foreign key linking to patron_quota.id
+
+=head2 issue_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+foreign key linking to issues.issue_id
+
+=head2 patron_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+foreign key linking to borrowers.borrowernumber
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "patron_quota_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "issue_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "patron_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 issue
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Issue>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "issue",
+  "Koha::Schema::Result::Issue",
+  { issue_id => "issue_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "SET NULL",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 patron
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Borrower>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "patron",
+  "Koha::Schema::Result::Borrower",
+  { borrowernumber => "patron_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 patron_quota
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::PatronQuota>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "patron_quota",
+  "Koha::Schema::Result::PatronQuota",
+  { id => "patron_quota_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2025-01-30 16:04:36
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:n1Hc6G8Z2fYHFlGpF4c/jg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
