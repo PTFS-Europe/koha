ef504bbbe282fe452eb62df2e0552222e83f5f30
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-01-30 10:39:13 -0100

    Bug 37893: DRY institutions

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/SIP2/SIP2InstitutionResource.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/SIP2/SIP2InstitutionResource.vue
@@ -4,6 +4,9 @@ import { APIClient } from "../../fetch/api-client.js"
 
 export default {
     extends: BaseResource,
+    props: {
+        routeAction: String,
+    },
     setup(props) {
         return {
             ...BaseResource.setup({
@@ -18,6 +21,8 @@ export default {
                 resourceTableUrl: APIClient.sip2._baseURL + "institutions",
                 i18n: {
                     displayName: __("Institution"),
+                    displayNameLowerCase: __("institution"),
+                    displayNamePlural: __("institutions"),
                 },
             }),
         }
@@ -31,6 +36,7 @@ export default {
                     type: "text",
                     label: __("Name"),
                     show_in_table: true,
+                    group: "Details",
                 },
                 {
                     name: "implementation",
@@ -38,24 +44,28 @@ export default {
                     type: "text",
                     label: __("Implementation"),
                     show_in_table: true,
+                    group: "Details",
                 },
                 {
                     name: "checkin",
                     type: "boolean",
                     label: __("Checkin"),
                     show_in_table: true,
+                    group: "Policy",
                 },
                 {
                     name: "checkout",
                     type: "boolean",
                     label: __("Checkout"),
                     show_in_table: true,
+                    group: "Policy",
                 },
                 {
                     name: "renewal",
                     type: "boolean",
                     label: __("Renewal"),
                     show_in_table: true,
+                    group: "Policy",
                 },
                 {
                     name: "retries",
@@ -63,6 +73,8 @@ export default {
                     type: "text",
                     label: __("Retries"),
                     show_in_table: true,
+                    group: "Policy",
+                    defaultValue: 5,
                 },
                 {
                     name: "status_update",
@@ -70,6 +82,7 @@ export default {
                     type: "boolean",
                     label: __("Status update"),
                     show_in_table: true,
+                    group: "Policy",
                 },
                 {
                     name: "timeout",
@@ -77,11 +90,136 @@ export default {
                     type: "text",
                     label: __("Timeout"),
                     show_in_table: true,
+                    group: "Policy",
+                    defaultValue: 100,
                 },
             ],
+            tableOptions: {
+                columns: this.getTableColumns(),
+                url: () => this.resourceTableUrl,
+                table_settings: this.institutions_table_settings,
+                actions: {
+                    0: ["show"],
+                    "-1": this.embedded
+                        ? [
+                              {
+                                  select: {
+                                      text: this.$__("Select"),
+                                      icon: "fa fa-check",
+                                  },
+                              },
+                          ]
+                        : ["edit", "delete"],
+                },
+            },
         }
     },
-    methods: {},
+    methods: {
+        getTableColumns: function () {
+            return [
+                {
+                    title: __("Name"),
+                    data: "name:sip_institution_id",
+                    searchable: true,
+                    orderable: true,
+                    render: function (data, type, row, meta) {
+                        return (
+                            '<a role="button" class="show">' +
+                            escape_str(
+                                `${row.name} (#${row.sip_institution_id})`
+                            ) +
+                            "</a>"
+                        )
+                    },
+                },
+                {
+                    title: __("Implementation"),
+                    data: "implementation",
+                    searchable: true,
+                    orderable: true,
+                },
+                {
+                    title: __("Checkin"),
+                    data: "checkin",
+                    searchable: true,
+                    orderable: true,
+                    render: function (data, type, row, meta) {
+                        return escape_str(row.checkin ? __("Yes") : __("No"))
+                    },
+                },
+                {
+                    title: __("Checkout"),
+                    data: "checkout",
+                    searchable: true,
+                    orderable: true,
+                    render: function (data, type, row, meta) {
+                        return escape_str(row.checkout ? __("Yes") : __("No"))
+                    },
+                },
+                {
+                    title: __("Renewal"),
+                    data: "renewal",
+                    searchable: true,
+                    orderable: true,
+                    render: function (data, type, row, meta) {
+                        return escape_str(row.renewal ? __("Yes") : __("No"))
+                    },
+                },
+                {
+                    title: __("Retries"),
+                    data: "retries",
+                    searchable: true,
+                    orderable: true,
+                },
+                {
+                    title: __("Status update"),
+                    data: "status_update",
+                    searchable: true,
+                    orderable: true,
+                    render: function (data, type, row, meta) {
+                        return escape_str(
+                            row.status_update ? __("Yes") : __("No")
+                        )
+                    },
+                },
+                {
+                    title: __("Timeout"),
+                    data: "timeout",
+                    searchable: true,
+                    orderable: true,
+                },
+            ]
+        },
+        onSubmit(e, institutionToSave) {
+            e.preventDefault()
+
+            let institution = JSON.parse(JSON.stringify(institutionToSave)) // copy
+            let sip_institution_id = institution.sip_institution_id
+
+            delete institution.sip_institution_id
+
+            const client = APIClient.sip2
+            if (sip_institution_id) {
+                client.institutions
+                    .update(institution, sip_institution_id)
+                    .then(
+                        success => {
+                            this.setMessage(this.$__("Institution updated"))
+                            this.$router.push({ name: "SIP2InstitutionsList" })
+                        },
+                        error => {}
+                    )
+            } else {
+                client.institutions.create(institution).then(
+                    success => {
+                        this.setMessage(this.$__("Institution created"))
+                        this.$router.push({ name: "SIP2InstitutionsList" })
+                    },
+                    error => {}
+                )
+            }
+        },
+    },
     name: "SIP2InstitutionResource",
 }
 </script>
