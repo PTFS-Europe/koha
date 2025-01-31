d4993f80a372ec93caafa696996650c256d4c91c
Jacob O'Mara <jacobomara901@gmail.com>
2025-01-31 18:11:50 +0000

    Bug 38924: Remove foreign key constraints from quota_usage table
    This removes the foreign key constraint from the quota usage table for
    issues_id as isssues are moved in to old_issues when a checkout is
    returned rendering this constraint useless.

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -6606,16 +6606,15 @@ DROP TABLE IF EXISTS `patron_quota_usage`;
 CREATE TABLE `patron_quota_usage` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'unique identifier for quota usage record',
   `patron_quota_id` int(11) NOT NULL COMMENT 'foreign key linking to patron_quota.id',
-  `issue_id` int(11) DEFAULT NULL COMMENT 'foreign key linking to issues.issue_id',
+  `issue_id` int(11) DEFAULT NULL COMMENT 'linking to issues.issue_id or old_issues.issue_id',
   `patron_id` int(11) NOT NULL COMMENT 'foreign key linking to borrowers.borrowernumber',
   PRIMARY KEY (`id`),
   KEY `patron_quota_usage_ibfk_1` (`patron_quota_id`),
-  KEY `patron_quota_usage_ibfk_2` (`issue_id`),
-  KEY `patron_quota_usage_ibfk_3` (`patron_id`),
+
+  KEY `patron_quota_usage_ibfk_2` (`patron_id`),
   CONSTRAINT `patron_quota_usage_ibfk_1` FOREIGN KEY (`patron_quota_id`) REFERENCES `patron_quota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `patron_quota_usage_ibfk_2` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`issue_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `patron_quota_usage_ibfk_3` FOREIGN KEY (`patron_id`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;     
+  CONSTRAINT `patron_quota_usage_ibfk_2` FOREIGN KEY (`patron_id`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;   
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
