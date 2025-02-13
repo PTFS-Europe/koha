e1fc7ee472f9890bff27aa8be13b225a8b9a4f71
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2025-02-13 09:41:29 +0000

    Bug 38010: (QA follow-up) Rebase for bug 37930

--- koha-tmpl/intranet-tmpl/prog/js/vue/stores/vendors.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/stores/vendors.js
@@ -1,4 +1,9 @@
 import { defineStore } from "pinia";
+import {
+    loadAuthorisedValues,
+    get_lib_from_av_handler,
+    map_av_dt_filter_handler,
+} from "../composables/authorisedValues";
 
 export const useVendorStore = defineStore("vendors", {
     state: () => ({
@@ -10,5 +15,18 @@ export const useVendorStore = defineStore("vendors", {
                 edifact: false,
             },
         },
+        authorisedValues: {
+            av_vendor_types: "VENDOR_TYPE",
+            av_vendor_interface_types: "VENDOR_INTERFACE_TYPE",
+        },
     }),
+    actions: {
+        loadAuthorisedValues,
+        get_lib_from_av(arr_name, av) {
+            return get_lib_from_av_handler(arr_name, av, this);
+        },
+        map_av_dt_filter(arr_name) {
+            return map_av_dt_filter_handler(arr_name, this);
+        },
+    },
 });
