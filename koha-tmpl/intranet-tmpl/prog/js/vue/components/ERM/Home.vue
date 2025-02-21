ff96a87c41db51c0810fdea10dbb9e3c947c5003
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-02-21 15:25:17 -0100

    Bug XYZ: Module Dashboards: Make ERM home extend ModuleDashboard

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
@@ -1,7 +1,8 @@
-<template></template>
-
 <script>
+import ModuleDashboard from "../ModuleDashboard/ModuleDashboard.vue";
+
 export default {
+    extends: ModuleDashboard,
     data() {
         return {};
     },
