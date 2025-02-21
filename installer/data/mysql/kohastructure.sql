a659540344d6dc5949c19d31576d5ba37be922e8
Jacob O'Mara <jacobomara901@gmail.com>
2025-02-21 15:49:18 +0000

    Bug 39224: Database updates for shibboleth
    This changes column names to make it easier to see what each column does
    at a glance, additionally remove the not null requirement from idp_field
    and add new default_content column

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -6767,9 +6767,10 @@ DROP TABLE IF EXISTS `shibboleth_field_mappings`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE shibboleth_field_mappings (
   mapping_id int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  idp_field varchar(255) NOT NULL COMMENT 'Field name from the identity provider',
+  idp_field varchar(255) COMMENT 'Field name from the identity provider',
   koha_field varchar(255) NOT NULL COMMENT 'Corresponding field in Koha borrowers table',
   is_matchpoint tinyint(1) NOT NULL DEFAULT 0 COMMENT 'If this field is used to match existing users',
+  default_content varchar(255) DEFAULT NULL COMMENT 'Default content for this field if not provided by the IdP',
   PRIMARY KEY (mapping_id),
   UNIQUE KEY koha_field_idx (koha_field),
   KEY idp_field_idx (idp_field)
@@ -6785,8 +6786,8 @@ DROP TABLE IF EXISTS `shibboleth_config`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE shibboleth_config (
   shibboleth_config_id int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  enable_opac_sso tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Enable Shibboleth SSO for OPAC',
-  enable_staff_sso tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Enable Shibboleth SSO for staff interface', 
+  force_opac_sso tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Force Shibboleth SSO for OPAC',
+  force_staff_sso tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Force Shibboleth SSO for staff interface', 
   autocreate tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Automatically create new patrons',
   sync tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Sync patron attributes on login',
   welcome tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Send welcome email to new patrons',
