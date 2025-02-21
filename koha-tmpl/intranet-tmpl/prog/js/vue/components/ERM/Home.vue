2a3ad03890abd303bced127dbff237f2849ba134
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-02-21 15:25:17 -0100

    Bug 39320: Add dashboard to ERM home

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
@@ -1,11 +1,24 @@
-<template></template>
-
 <script>
+import ModuleDashboard from "../ModuleDashboard/ModuleDashboard.vue";
+import ERMCounts from "../ModuleDashboard/Widgets/ERMCounts.vue";
+import ERMLicensesNeedingAction from "../ModuleDashboard/Widgets/ERMLicensesNeedingAction.vue";
+import ERMRunUsageReport from "../ModuleDashboard/Widgets/ERMRunUsageReport.vue";
+import ERMLatestSUSHIJobs from "../ModuleDashboard/Widgets/ERMLatestSUSHIJobs.vue";
+import { markRaw } from "vue";
+
 export default {
-    data() {
-        return {};
+    extends: ModuleDashboard,
+    setup() {
+        return {
+            ...ModuleDashboard.setup({
+                availableWidgets: [
+                    markRaw(ERMRunUsageReport),
+                    markRaw(ERMLatestSUSHIJobs),
+                    markRaw(ERMCounts),
+                    markRaw(ERMLicensesNeedingAction),
+                ],
+            }),
+        };
     },
-    methods: {},
-    components: {},
 };
 </script>
