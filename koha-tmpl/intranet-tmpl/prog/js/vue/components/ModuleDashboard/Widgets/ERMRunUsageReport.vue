06a8110dbd2187ec2b34dda124148c93398cd488
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-02-26 12:44:41 -0100

    more polish work

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ModuleDashboard/Widgets/ERMRunUsageReport.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ModuleDashboard/Widgets/ERMRunUsageReport.vue
@@ -12,27 +12,47 @@
     <template v-else-if="display === 'dashboard'">
         <WidgetDashboardWrapper @removed="removeWidget" :name="name">
             <template #default>
-                <div class="d-flex align-items-center">
-                    <div class="flex-grow-1 me-2">
-                        <label for="filter" class="visually-hidden"
-                            >Pick a report to run</label
-                        >
-                        <v-select
-                            label="report_name"
-                            :options="default_usage_reports"
-                            style="min-width: 200px"
-                            v-model="selected_report"
-                        ></v-select>
-                    </div>
-                    <div>
+                <div v-if="loading" class="text-center">
+                    {{ $__("Loading...") }}
+                </div>
+                <div v-else>
+                    <div v-if="!default_usage_reports.length">
+                        <div class="d-flex align-items-center alert alert-info">
+                            {{
+                                $__(
+                                    "No saved eUsage reports are available to run."
+                                )
+                            }}
+                        </div>
                         <button
                             class="btn btn-primary"
                             type="button"
-                            :disabled="!selected_report"
-                            @click="runReport"
+                            @click="createReport"
                         >
-                            Run
+                            {{ $__("Create a report") }}
                         </button>
+                    </div>
+                    <div v-else class="d-flex align-items-center">
+                        <div class="flex-grow-1 me-2">
+                            <label for="filter" class="visually-hidden">{{
+                                $__("Pick a report to run")
+                            }}</label>
+                            <v-select
+                                label="report_name"
+                                :options="default_usage_reports"
+                                style="min-width: 200px"
+                                v-model="selected_report"
+                            ></v-select>
+                        </div>
+                        <div>
+                            <button
+                                class="btn btn-primary"
+                                type="button"
+                                @click="runReport"
+                            >
+                                {{ $__("Run") }}
+                            </button>
+                        </div>
                     </div>
                 </div>
             </template>
@@ -64,12 +84,13 @@ export default {
         const default_usage_reports = ref([]);
         const selected_report = ref(null);
         const reportsLoaded = ref(false);
-
+        const loading = ref(true);
         const getReports = async () => {
             try {
                 const response =
                     await APIClient.erm.default_usage_reports.getAll();
                 default_usage_reports.value = response;
+                loading.value = false;
             } catch (error) {
                 console.error("Error getting default usage reports", error);
             }
@@ -84,6 +105,13 @@ export default {
             });
         };
 
+        const createReport = e => {
+            e.preventDefault();
+            router.push({
+                name: "UsageStatisticsReportsHome",
+            });
+        };
+
         onMounted(() => {
             if (!reportsLoaded.value) {
                 getReports();
@@ -95,7 +123,9 @@ export default {
             default_usage_reports,
             selected_report,
             getReports,
+            createReport,
             runReport,
+            loading,
             name,
             description,
         };
