fa0cc3e83a595251ceeaf9b4a1e2b8bf881cd728
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2025-03-11 13:27:43 +0000

    Ticket 24436: Update for more regular running of edi_cron
    The edi cron on UAL runs every 10 minutes rather than once a day, thus
    breaking some assumptions the original fix script had.
    
    This patch updates the fixup script such that is uses edifact_messages
    of the type 'QUOTE' with the statuses of 'received' or 'recmsg' and then
    updates the status to 'post-$status' to track those quotes that have
    been post processed.

--- misc/cronjobs/edi_fix.pl
+++ misc/cronjobs/edi_fix.pl
@@ -19,7 +19,7 @@ use DateTime;
 
 use Koha::Script -cron;
 use C4::Context;
-use Koha::Acquisition::Orders;
+use Koha::Acquisition::Basket;
 use Koha::Database;
 use Koha::Logger;
 
@@ -30,62 +30,74 @@ my $schema = Koha::Database->new()->schema();
 process_quotes();
 
 sub process_quotes {
-    my $today  = DateTime->now->ymd;
-    my $orders = Koha::Acquisition::Orders->search(
+    my $today    = DateTime->now->ymd;
+    my $messages = $schema->resultset('EdifactMessage')->search(
         {
-            'me.entrydate' => $today,
-        },
+            message_type => 'QUOTE',
+            status       => { '-in' => [ 'received', 'recmsg' ] }
+        }
     );
 
-    while ( my $order = $orders->next ) {
-        my $vendor_note = $order->order_vendornote;
+    while ( my $message = $messages->next ) {
+        my $basket_rs = $message->basketno;
+        my $basket    = Koha::Acquisition::Basket->_new_from_dbic($basket_rs);
 
-        # Process fields quoted by '$'
-        # Subject fund -> sort1
-        if ( my @matches = $vendor_note =~ /\$(.*?)\$/g ) {
-            if ( @matches > 1 ) {
-                warn "Multiple fields quoted by \$ found: @matches\n";
-            } else {
-                my $sort1 = $matches[0];
-                $order->sort1($sort1);
+        my $orders = $basket->orders;
+        while ( my $order = $orders->next ) {
+
+            my $vendor_note = $order->order_vendornote;
+
+            # Process fields quoted by '$'
+            # Subject fund -> sort1
+            if ( my @matches = $vendor_note =~ /\$(.*?)\$/g ) {
+                if ( @matches > 1 ) {
+                    warn "Multiple fields quoted by \$ found: @matches\n";
+                } else {
+                    my $sort1 = $matches[0];
+                    $order->sort1($sort1);
+                }
             }
+
+            # Process fields quoted by '^'
+            # Internal reference -> order_internalnote
+            if ( my @matches = $vendor_note =~ /\^(.*?)\^/g ) {
+                if ( @matches > 1 ) {
+                    warn "Multiple fields quoted by ^ found: @matches\n";
+                } else {
+                    my $order_internalnote =
+                        $order->order_internalnote ? $order->order_internalnote . ' ' . $matches[0] : $matches[0];
+                    $order->order_internalnote($order_internalnote);
+                }
+            }
+
+            # Process fields quoted by '!'
+            # Notes to ASM -> order_internalnote
+            if ( my @matches = $vendor_note =~ /!(.*?)!/g ) {
+                if ( @matches > 1 ) {
+                    warn "Multiple fields quoted by ^ found: @matches\n";
+                } else {
+                    my $order_internalnote =
+                        $order->order_internalnote ? $order->order_internalnote . ' ' . $matches[0] : $matches[0];
+                    $order->order_internalnote($order_internalnote);
+                }
+            }
+
+            # Process fields quoted by '#'
+            # Purhchase reason -> sort2
+            if ( my @matches = $vendor_note =~ /\#(.*?)\#/g ) {
+                if ( @matches > 1 ) {
+                    warn "Multiple fields quoted by # found: @matches\n";
+                } else {
+                    my $sort2 = $matches[0];
+                    $order->sort2($sort2);
+                }
+            }
+
+            $order->store();
         }
 
-        # Process fields quoted by '^'
-        # Internal reference -> order_internalnote
-        if ( my @matches = $vendor_note =~ /\^(.*?)\^/g ) {
-            if ( @matches > 1 ) {
-                warn "Multiple fields quoted by ^ found: @matches\n";
-            } else {
-                my $order_internalnote =
-                    $order->order_internalnote ? $order->order_internalnote . ' ' . $matches[0] : $matches[0];
-                $order->order_internalnote($order_internalnote);
-            }
-        }
-
-        # Process fields quoted by '!'
-        # Notes to ASM -> order_internalnote
-        if ( my @matches = $vendor_note =~ /!(.*?)!/g ) {
-            if ( @matches > 1 ) {
-                warn "Multiple fields quoted by ^ found: @matches\n";
-            } else {
-                my $order_internalnote =
-                    $order->order_internalnote ? $order->order_internalnote . ' ' . $matches[0] : $matches[0];
-                $order->order_internalnote($order_internalnote);
-            }
-        }
-
-        # Process fields quoted by '#'
-        # Purhchase reason -> sort2
-        if ( my @matches = $vendor_note =~ /\#(.*?)\#/g ) {
-            if ( @matches > 1 ) {
-                warn "Multiple fields quoted by # found: @matches\n";
-            } else {
-                my $sort2 = $matches[0];
-                $order->sort2($sort2);
-            }
-        }
-
-        $order->store();
+        my $status = 'post-' . $message->status;
+        $message->status($status);
+        $message->update;
     }
 }
